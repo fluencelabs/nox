949a4e49343633e4b2cd23933a84310220952a34
alari <name.alari@gmail.com>
2017-10-24 14:09:25 +0300

    network: simple server conf

--- build.sbt
+++ build.sbt
@@ -21,31 +21,23 @@ scalaV
 
 scalacOptions += "-Ypartial-unification"
 
-val FreesV = "0.3.1"
-val MonixV = "2.3.0"
 val RocksDbV = "5.8.0"
 val TypeSafeConfV = "1.3.2"
 val FicusV = "1.4.2"
 val MockitoV = "2.11.0"
 
-val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
-val log4j2 = "org.apache.logging.log4j" %% "log4j-api-scala" % "2.9.1"
-val scalatest = "org.scalatest" %% "scalatest" % "3.0.3" % Test
-val frees = "io.frees" %% "freestyle" % FreesV
-val monix = "io.monix" %% "monix" % MonixV
-val monixCats = "io.monix" %% "monix-cats" % MonixV
-val cats = "org.typelevel" %% "cats" % "0.9.0"
+val logback = "ch.qos.logback" % "logback-classic" % "1.2.+"
 
 val cats1 = "org.typelevel" %% "cats-core" % "1.0.0-MF"
 val monix3 = "io.monix" %% "monix" % "3.0.0-M1"
-val shapeless = "com.chuusai" %% "shapeless" % "2.3.2"
+val shapeless = "com.chuusai" %% "shapeless" % "2.3.+"
 
 val rocksDb = "org.rocksdb" % "rocksdbjni" % RocksDbV
 val typeSafeConfig = "com.typesafe" % "config" % TypeSafeConfV
 val ficus = "com.iheart" %% "ficus" % FicusV
-val mockito = "org.mockito" % "mockito-core" % MockitoV % Test
 
-val paradise = addCompilerPlugin("org.scalameta" % "paradise" % "3.0.0-M10" cross CrossVersion.full)
+val mockito = "org.mockito" % "mockito-core" % MockitoV % Test
+val scalatest = "org.scalatest" %% "scalatest" % "3.0.+" % Test
 
 val grpc = Seq(
   PB.targets in Compile := Seq(
@@ -61,11 +53,7 @@ val grpc = Seq(
 lazy val `fluence` = project.in(file("."))
   .settings(
     scalaV,
-    paradise,
     libraryDependencies ++= Seq(
-      frees,
-      monix,
-      monixCats,
       scalatest
     )
   ).aggregate(
@@ -104,6 +92,8 @@ lazy val `network` = project.in(file("network"))
   libraryDependencies ++= Seq(
     monix3,
     shapeless,
+    typeSafeConfig,
+    ficus,
     "org.bitlet" % "weupnp" % "0.1.+",
     scalatest
   )
--- network/src/main/scala/fluence/network/server/NetworkServer.scala
+++ network/src/main/scala/fluence/network/server/NetworkServer.scala
@@ -7,15 +7,26 @@ import io.grpc.{ Server, ServerBuilder, ServerServiceDefinition }
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits._
 
+/**
+ * Server wrapper
+ * @param server grpc Server instance
+ * @param contact Self-discovered contact of this server
+ * @param onShutdown Callback to launch on shutdown
+ */
 class NetworkServer private (
-    server:     Server,
-    contact:    Task[Contact],
-    onShutdown: Task[Unit]
+    server:      Server,
+    val contact: Task[Contact],
+    onShutdown:  Task[Unit]
 ) {
-  def start(): Unit = {
-    server.start()
-  }
+  /**
+   * Launch server, grab ports, or fail
+   */
+  def start(): Task[Unit] =
+    Task(server.start())
 
+  /**
+   * Shut the server down, release ports
+   */
   def shutdown(): Unit = {
     server.shutdown()
     onShutdown.runSyncMaybe
@@ -25,10 +36,27 @@ class NetworkServer private (
 }
 
 object NetworkServer {
+
+  /**
+   * Builder for grpc network server
+   * @param contact Self-discovered contact for current node
+   * @param shutdown What to call back on shutdown
+   * @param localPort Local port to launch server on
+   * @param services GRPC services definitions
+   */
   class Builder(val contact: Task[Contact], shutdown: Task[Unit], localPort: Int, services: List[ServerServiceDefinition]) {
+    /**
+     * Add new grpc service to the server
+     * @param service Service definition
+     * @return
+     */
     def add(service: ServerServiceDefinition): Builder =
       new Builder(contact, shutdown, localPort, service :: services)
 
+    /**
+     * Build grpc server with all the defined services
+     * @return
+     */
     def build: NetworkServer =
       new NetworkServer(
         {
@@ -42,11 +70,38 @@ object NetworkServer {
       )
   }
 
+  /**
+   * Builder for a local port, no upnp
+   * @param localPort Local port
+   * @return
+   */
   def builder(localPort: Int): Builder =
     new Builder(Task.now(Contact(InetAddress.getLocalHost, localPort)), Task.unit, localPort, Nil)
 
+  /**
+   * Builder for a local port, with upnp used to provide external port
+   * @param localPort Local port
+   * @param externalPort External port to be grabbed on gateway device
+   * @param uPnP UPnP instance to use for gateway management
+   * @return
+   */
   def builder(localPort: Int, externalPort: Int, uPnP: UPnP = new UPnP()): Builder =
     new Builder(uPnP.addPort(externalPort, localPort).memoizeOnSuccess.onErrorRecover{
       case _ ⇒ Contact(InetAddress.getLocalHost, localPort)
     }, uPnP.deletePort(externalPort).memoizeOnSuccess, localPort, Nil)
+
+  /**
+   * Builder for config object
+   * @param conf Server config object
+   * @return
+   */
+  def builder(conf: ServerConf): Builder =
+    conf.externalPort.fold(builder(conf.localPort))(ext ⇒ builder(conf.localPort, ext))
+
+  /**
+   * Builder for default config object, read from typesafe conf
+   * @return
+   */
+  def builder: Builder =
+    builder(ServerConf.read())
 }
