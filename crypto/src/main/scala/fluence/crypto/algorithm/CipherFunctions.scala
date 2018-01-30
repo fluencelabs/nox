2380352a26d5a26bdeacaabf60674a50c6b0a94e
dmitry.shakhtarin <dmitry.shakhtarin@fluence.ai>
2018-01-30 16:11:50 +0300

    add fluence headers

--- crypto/src/main/scala/fluence/crypto/algorithm/CipherFunctions.scala
+++ crypto/src/main/scala/fluence/crypto/algorithm/CipherFunctions.scala
@@ -1,9 +1,25 @@
+/*
+ * Copyright (C) 2017  Fluence Labs Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fluence.crypto.algorithm
 
 import fluence.crypto.keypair.KeyPair
-import scodec.bits.ByteVector
 
 trait CipherFunctions extends Algorithm {
-  def encrypt(keyPair: KeyPair, message: ByteVector): ByteVector
-  def decrypt(keyPair: KeyPair, message: ByteVector): ByteVector
+  def encrypt(keyPair: KeyPair, message: Array[Byte]): Array[Byte]
+  def decrypt(keyPair: KeyPair, message: Array[Byte]): Array[Byte]
 }
