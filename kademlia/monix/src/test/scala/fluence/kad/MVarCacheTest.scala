361811a1979ea8ae8be6fbf35563bcc33c564e3f
dmitry.shakhtarin <dmitry.shakhtarin@fluence.ai>
2018-02-16 12:17:55 +0300

    update test

--- kademlia/monix/src/test/scala/fluence/kad/MVarCacheTest.scala
+++ kademlia/monix/src/test/scala/fluence/kad/MVarCacheTest.scala
@@ -11,6 +11,7 @@ import scala.util.{ Failure, Success }
 class MVarCacheTest extends WordSpec with Matchers with ScalaFutures {
 
   //todo move it to utility module
+  //todo add real concurrent tests
   private implicit class WaitTask[T](task: Task[T]) {
     def taskValue()(implicit s: Scheduler): T = {
       val future: CancelableFuture[T] = task.runAsync(s)
@@ -26,22 +27,40 @@ class MVarCacheTest extends WordSpec with Matchers with ScalaFutures {
     "work" in {
       val cache = new MVarMapCache[Int, String]("")
 
-      cache.getOrAdd(1, "1").taskValue() shouldBe "1"
+      Task.sequence(Seq(
+        cache.getOrAdd(1, "1"),
+        cache.getOrAdd(1, "2"),
+        cache.getOrAdd(1, "3")
+      )).taskValue()
 
-      cache.getOrAdd(1, "2").taskValue() shouldBe "1"
+      cache.get(1).get shouldBe "1"
 
-      cache.modify(1, s ⇒ s ++ "2").taskValue() shouldBe true
-      cache.get(1).get shouldBe "12"
+      Task.sequence(Seq(
+        cache.modify(1, s ⇒ s ++ "2"),
+        cache.modify(1, s ⇒ s ++ "3"),
+        cache.modify(1, s ⇒ s ++ "4")
+      )).taskValue()
 
-      cache.update(1, "123").taskValue()
-      cache.get(1).get shouldBe "123"
+      cache.get(1).get shouldBe "1234"
+
+      Task.sequence(Seq(
+        cache.update(1, "111"),
+        cache.update(1, "222"),
+        cache.update(1, "333")
+      )).taskValue()
+
+      cache.get(1).get shouldBe "333"
 
       cache.modify(12, s ⇒ s ++ "2").taskValue() shouldBe false
 
-      cache.getOrAddF(1, Task.pure("2")).taskValue() shouldBe "123"
+      cache.getOrAddF(1, Task.pure("2")).taskValue() shouldBe "333"
 
-      cache.getOrAddF(4, Task.pure("4")).taskValue() shouldBe "4"
-
+      Task.sequence(Seq(
+        cache.getOrAddF(4, Task.pure("444")),
+        cache.getOrAddF(4, Task.pure("555")),
+        cache.getOrAddF(4, Task.pure("666"))
+      )).taskValue()
+      cache.getOrAddF(4, Task.pure("4")).taskValue() shouldBe "444"
     }
   }
 }
