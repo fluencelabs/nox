488d323701395bc36746517fb01952f224abcb01
Constantine Solovev <constantine.solovev@gmail.com>
2018-03-20 13:13:07 +0400

    increase timeout

--- node/src/test/scala/fluence/node/ClientNodeIntegrationSpec.scala
+++ node/src/test/scala/fluence/node/ClientNodeIntegrationSpec.scala
@@ -412,7 +412,7 @@ class ClientNodeIntegrationSpec extends WordSpec with Matchers with ScalaFutures
     val allRecords = Random.shuffle(1 to numberOfKeys).map(i ⇒ { f"k$i%04d" → f"v$i%04d" })
 
     // invoke numberOfKeys puts
-    Task.sequence(allRecords.map { case (k, v) ⇒ datasetStorage.put(k, v) }).taskValue(Some(timeout(Span(15, Seconds))))
+    Task.sequence(allRecords.map { case (k, v) ⇒ datasetStorage.put(k, v) }).taskValue(Some(timeout(Span(20, Seconds))))
 
     val firstKey = "k0001"
     val midKey = f"k${numberOfKeys / 2}%04d"
