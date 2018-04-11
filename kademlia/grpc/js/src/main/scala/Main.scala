14baa25d8ae12b37d0597fe2edde878c7b679e5c
dmitry.shakhtarin <dmitry.shakhtarin@fluence.ai>
2018-04-12 00:42:16 +0300

    clean example main class

--- kademlia/grpc/js/src/main/scala/Main.scala
+++ kademlia/grpc/js/src/main/scala/Main.scala
@@ -43,17 +43,13 @@ object Main extends slogging.LazyLogging {
 
   @JSExport
   def logic(): Unit = {
-    println("Hello world!")
     val keyP = algo.generateKeyPair().value.toOption.get
     val key = Key.fromPublicKey(keyP.publicKey).value.toOption.get
     val io = for {
       node ← client.ping()
       _ = logger.info("Ping node response: " + node)
-      _ = println("Ping node response: " + node)
-
       listOfNodes ← client.lookup(key, 2)
       _ = logger.info("Lookup nodes response: " + listOfNodes.mkString("\n"))
-      _ = println("Lookup nodes response: " + listOfNodes.mkString("\n"))
       key2 = listOfNodes.head.key
       listOfNodes2 ← client.lookupAway(key2, key, 2)
     } yield {
