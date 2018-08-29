b4fa2ac8032f68dca3d41ea2560dbd0aec666b08
folex <0xdxdy@gmail.com>
2018-08-29 11:18:25 +0300

    Deployer.sol contract for bootstrapping cluster of Solvers (#47) [skip ci]

--- bootstrap/migrations/1_initial_migration.js
+++ bootstrap/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var Deployer = artifacts.require("./Deployer.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Deployer);
   deployer.deploy(Migrations);
 };
--- bootstrap/truffle.js
+++ bootstrap/truffle.js
@@ -3,11 +3,9 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "0.0.0.0",
       port: 8545,
-      from: "0x96dce7eb99848e3332e38663a1968836ba3c3b53",
-      gas: 4600000,
-      network_id: "1234" // Match any network id
+      network_id: "*" // Match any network id
     }
   }
 };
