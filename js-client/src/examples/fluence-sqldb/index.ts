ac4aafcbdf7cdcd032f49a667adeb99fbc907c6e
DieMyst <dmitry.shakhtarin@fluence.ai>
2018-10-10 08:46:06 +0300

    export all in main file

--- js-client/src/examples/fluence-sqldb/index.ts
+++ js-client/src/examples/fluence-sqldb/index.ts
@@ -1,37 +1,20 @@
-import {Session} from "js-fluence-client/dist/Session";
-import {Client} from "js-fluence-client/dist/Client";
-import {TendermintClient} from "js-fluence-client/dist/TendermintClient";
-import {Engine} from "js-fluence-client/dist/Engine";
-import {Signer} from "js-fluence-client/dist/Signer";
-import {Empty, isValue, Value} from "js-fluence-client/dist/Result";
-import {fromHex} from "js-fluence-client/dist/utils";
 import "bootstrap/dist/css/bootstrap.min.css";
+import * as fluence from "js-fluence-client";
 
 
 class DbClient {
 
-    session: Session;
+    session: fluence.Session;
 
     constructor(host: string, port: number) {
-        let tm = new TendermintClient(host, port);
-
-        let engine = new Engine(tm);
-
-        // default signing key for now
-        let signingKey = "TVAD4tNeMH2yJfkDZBSjrMJRbavmdc3/fGU2N2VAnxT3hAtSkX+Lrl4lN5OEsXjD7GGG7iEewSod472HudrkrA==";
-        let signer = new Signer(signingKey);
-
-        // `client002` is a default client for now
-        let client = new Client("client002", signer);
-
-        this.session = engine.genSession(client);
+        this.session = fluence.createDefaultSession(host, port)
     }
 
-    async submitQuery(query: string): Promise<Empty | Value> {
+    async submitQuery(query: string): Promise<fluence.Empty | fluence.Value> {
         let command = `do_query("${query}")`;
         let res = await this.session.invokeRaw(command).result();
-        if (isValue(res)) {
-            let strResult = fromHex(res.hex());
+        if (fluence.isValue(res)) {
+            let strResult = fluence.fromHex(res.hex());
             console.log(`the result is:\n ${strResult}`);
         } else {
             console.log(`the result is empty`);
@@ -58,9 +41,9 @@ if (btn !== null) {
 export function submitQuery(query: string) {
     client.submitQuery(query).then((res) => {
 
-        if (isValue(res) && resultField !== null) {
+        if (fluence.isValue(res) && resultField !== null) {
             let newLine = String.fromCharCode(13, 10);
-            let str = fromHex(res.hex());
+            let str = fluence.fromHex(res.hex());
             resultField.value = str.replace('\\n', newLine);
         }
     })
