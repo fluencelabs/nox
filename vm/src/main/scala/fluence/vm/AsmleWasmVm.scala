043fd2aec8da111d8bcc9294c220848c9223d42b
vms <michail.vms@gmail.com>
2018-10-12 10:09:22 +0300

    add the first version of array supplied variant

--- vm/src/main/scala/fluence/vm/AsmleWasmVm.scala
+++ vm/src/main/scala/fluence/vm/AsmleWasmVm.scala
@@ -17,15 +17,14 @@
 package fluence.vm
 import java.lang.reflect.Method
 import java.nio.ByteOrder
-import java.nio.charset.Charset
 
 import asmble.compile.jvm.AsmExtKt
 import cats.data.EitherT
 import cats.effect.{IO, LiftIO}
-import cats.{Functor, Monad, Traverse}
+import cats.{Functor, Monad}
 import fluence.crypto.Crypto.Hasher
 import fluence.vm.VmError.WasmVmError.{GetVmStateError, InvokeError}
-import fluence.vm.VmError.{InvalidArgError, _}
+import fluence.vm.VmError.{NoSuchFnError, _}
 import fluence.vm.AsmleWasmVm._
 import scodec.bits.ByteVector
 
@@ -64,14 +63,10 @@ class AsmleWasmVm(
   private val deallocateFunction: Option[WasmFunction] =
     functionsIndex.get(FunctionId(modules.head.name, AsmExtKt.getJavaIdent(deallocateFunctionName)))
 
-  // TODO: it is need to decide how to properly get charset (maybe it is good to add "getCharset" method
-  // in all wasm Modules or simply add the requirement of using fixed charset everywhere)
-  private val wasmStringCharset: Charset = Charset.forName("UTF-8")
-
   override def invoke[F[_]: LiftIO: Monad](
     moduleName: Option[String],
     fnName: String,
-    fnArgs: Seq[String]
+    fnArgument: Option[Array[Byte]]
   ): EitherT[F, InvokeError, Option[Array[Byte]]] = {
     val functionId = FunctionId(moduleName, AsmExtKt.getJavaIdent(fnName))
 
@@ -83,17 +78,13 @@ class AsmleWasmVm(
           NoSuchFnError(s"Unable to find a function with the name=$functionId")
         )
 
-      preprocessedArguments <- preprocessStringArguments(fnArgs, wasmFn.module, wasmStringCharset)
-      arguments ← parseArguments(preprocessedArguments, wasmFn)
+      preprocessedArguments = fnArgument match {
+        case Some(arg) => injectArrayIntoWasmModule(arg, wasmFn.module).asInstanceOf[AnyRef] :: arg.length.asInstanceOf[AnyRef] :: Nil
+        case _ => Nil
+      }
 
       // invoke the function
-      invocationResult ← wasmFn[F](arguments)
-      // TODO : In the current version it is expected that callee (Wasm module) clean memory by itself
-      // after construction of the result string. F.e. in Rust String object along with &str are the common
-      // classes for operations on strings, in C++ the same role has std::string and std::stringview classes.
-      // So, now it is expected that Rust/C++ methods at first construct objects of corresponding classes by
-      // using injected string bytes as source and then delete these string bytes. But in the future it has
-      // to be done by caller through deallocate function call.
+      invocationResult ← wasmFn[F](preprocessedArguments)
 
       extractedResult <- if (wasmFn.javaMethod.getReturnType == Void.TYPE) {
         EitherT.rightT[F, InvokeError](None)
@@ -119,7 +110,7 @@ class AsmleWasmVm(
 
             prevModulesHash ← acc
 
-            concatHashes ← safeConcat(moduleStateHash, prevModulesHash)
+            concatHashes = Array.concat(moduleStateHash, prevModulesHash)
 
             resultHash ← hasher(concatHashes)
               .leftMap(e ⇒ InternalVmError(s"Getting VM state for module=$instance failed", Some(e)): GetVmStateError)
@@ -155,59 +146,22 @@ class AsmleWasmVm(
     deallocateFunction match {
       case Some(fn) => fn(offset.asInstanceOf[AnyRef] :: size.asInstanceOf[AnyRef] :: Nil)
       case _ =>
-        EitherT.leftT(
+       EitherT.leftT(
           NoSuchFnError(s"Unable to find the function for memory deallocation with the name=$deallocateFunctionName")
         )
     }
   }
 
   /**
-   * Preprocesses each string parameter: injects it into Wasm module memory (through
-   * injectStringIntoWasmModule) and replace with pointer to it in WASM module and size.
-   *
-   * @param functionArguments arguments for calling this fn
-   * @param moduleInstance module instance used for injecting string arguments to the Wasm memory
-   * @param charset charset that is used for convert the string to byte array
-   * @tparam F a monad with an ability to absorb 'IO'
-   */
-  private def preprocessStringArguments[F[_]: LiftIO: Monad](
-    functionArguments: Seq[String],
-    moduleInstance: ModuleInstance,
-    charset: Charset
-  ): EitherT[F, InvokeError, List[String]] = {
-    val result: Seq[EitherT[F, InvokeError, List[String]]] =
-      functionArguments.map {
-        case stringArgument
-            if stringArgument.startsWith("\"")
-              && stringArgument.endsWith("\"")
-              && stringArgument.length >= 2 ⇒
-          // it is a valid String parameter
-          for {
-            address <- injectStringIntoWasmModule(
-              stringArgument.substring(1, stringArgument.length - 1),
-              moduleInstance,
-              charset
-            )
-          } yield List(address.toString, (stringArgument.length - 2).toString)
-        case arg ⇒ EitherT.rightT[F, InvokeError](List(arg))
-      }
-
-    import cats.instances.list._
-    Traverse[List].flatSequence(result.toList)
-  }
-
-  /**
    * Injects given string into Wasm module memory.
    *
-   * @param injectedString string that should be inserted into Wasm module memory
+   * @param injectedArray array that should be inserted into Wasm module memory
    * @param moduleInstance module instance used as a provider for Wasm module memory access
-   * @param charset charset that is used for convert the string to byte array
    * @tparam F a monad with an ability to absorb 'IO'
    */
-  private def injectStringIntoWasmModule[F[_]: LiftIO: Monad](
-    injectedString: String,
-    moduleInstance: ModuleInstance,
-    charset: Charset
+  private def injectArrayIntoWasmModule[F[_]: LiftIO: Monad](
+    injectedArray: Array[Byte],
+    moduleInstance: ModuleInstance
   ): EitherT[F, InvokeError, Int] =
     for {
       // In the current version, it is possible for Wasm module to have allocation/deallocation
@@ -216,10 +170,10 @@ class AsmleWasmVm(
       // due to possible absence of this function in the Wasm module.
       wasmMemory <- EitherT.fromOption(
         moduleInstance.memory,
-        VmMemoryError(s"Trying to use absent Wasm memory while injecting string=$injectedString")
+        VmMemoryError(s"Trying to use absent Wasm memory while injecting array=$injectedArray")
       )
 
-      offset <- allocate(injectedString.length)
+      offset <- allocate[F](injectedArray.length)
 
       resultOffset <- EitherT
         .fromEither(Try {
@@ -227,7 +181,7 @@ class AsmleWasmVm(
           val wasmMemoryView = wasmMemory.duplicate()
 
           wasmMemoryView.position(convertedOffset)
-          wasmMemoryView.put(injectedString.getBytes(charset.name()))
+          wasmMemoryView.put(injectedArray)
 
           convertedOffset
         }.toEither)
@@ -253,7 +207,7 @@ class AsmleWasmVm(
       // TODO : string deallocation from scala-part should be additionally investigated - it seems
       // that this version of deletion doesn't compatible with current idea of verification game
       intBytesSize = 4
-      _ <- deallocate(offset, extractedResult.length + intBytesSize)
+      _ <- deallocate[F](offset, extractedResult.length + intBytesSize)
     } yield extractedResult
 
   /**
@@ -317,13 +271,12 @@ object AsmleWasmVm {
    * and java method [[java.lang.reflect.Method]].
    *
    * @param functionId a full name of this function (moduleName + functionName)
-   * @param javaMethod a java method [[java.lang.reflect.Method]] for calling fn.
+   * @param javaMethod a java method [[java.lang.reflect.Method]] for calling function.
    * @param module the object the underlying method is invoked from.
    *               This is an instance for the current module, it contains
    *               all inner state of the module, like memory.
    */
   case class WasmFunction(
-    functionId: FunctionId,
     javaMethod: Method,
     module: ModuleInstance
   ) {
@@ -331,99 +284,14 @@ object AsmleWasmVm {
     /**
      * Invokes this function with arguments.
      *
-     * @param args arguments for calling this fn.
+     * @param args arguments for calling this function.
      * @tparam F a monad with an ability to absorb 'IO'
      */
     def apply[F[_]: Functor: LiftIO](args: List[AnyRef]): EitherT[F, InvokeError, AnyRef] =
       EitherT(IO(javaMethod.invoke(module.instance, args: _*)).attempt.to[F])
         .leftMap(e ⇒ TrapError(s"Function $this with args: $args was failed", Some(e)))
 
-    override def toString: String = functionId.toString
-  }
-
-  /** Transforms specified ''arg'' with ''castFn'', returns ''errorMsg'' otherwise. */
-  private def cast[F[_]: Monad](
-    arg: String,
-    castFn: String ⇒ Any,
-    errorMsg: String
-  ): Either[InvalidArgError, AnyRef] =
-    // it's important to cast to AnyRef type, because args should be used in
-    // Method.invoke(...) as Object type
-    Try(castFn(arg).asInstanceOf[AnyRef]).toEither.left
-      .map(e ⇒ InvalidArgError(errorMsg, Some(e)))
-
-  /** Safe array concatenation. Prevents array overflow or OOM. */
-  private def safeConcat[F[_]: LiftIO: Monad](
-    first: Array[Byte],
-    second: Array[Byte]
-  ): EitherT[F, InternalVmError, Array[Byte]] = {
-    EitherT.fromEither {
-      Try(Array.concat(second, first)).toEither
-    }.leftMap(e ⇒ InternalVmError("Arrays concatenation failed", Some(e)))
-  }
-
-  private def parseArguments[F[_]: LiftIO: Monad](
-    fnArgs: List[String],
-    wasmFn: WasmFunction
-  ): EitherT[F, InvalidArgError, List[AnyRef]] = {
-
-    // Maps args to params
-    val required = wasmFn.javaMethod.getParameterTypes.length
-
-    for {
-      _ ← EitherT.cond(
-        required == fnArgs.size,
-        (),
-        InvalidArgError(
-          s"Invalid number of arguments, expected=$required, actually=${fnArgs.size} for fn=$wasmFn " +
-            s"(or passed string argument is incorrect or isn't matched the corresponding argument in Wasm function)"
-        )
-      )
-
-      args = wasmFn.javaMethod.getParameterTypes.zipWithIndex.zip(fnArgs).map {
-        case ((paramType, index), arg) =>
-          paramType match {
-            case cl if cl == classOf[Int] ⇒
-              cast(
-                arg,
-                _.toInt,
-                s"Arg $index of '$arg' not an int " +
-                  s"(or passed string argument isn't matched the corresponding argument in Wasm function)"
-              )
-            case cl if cl == classOf[Long] ⇒
-              cast(
-                arg,
-                _.toLong,
-                s"Arg $index of '$arg' not a long " +
-                  s"(or passed string argument isn't matched the corresponding argument in Wasm function)"
-              )
-            case cl if cl == classOf[Float] ⇒
-              cast(
-                arg,
-                _.toFloat,
-                s"Arg $index of '$arg' not a float " +
-                  s"(or passed string argument isn't matched the corresponding argument in Wasm function)"
-              )
-            case cl if cl == classOf[Double] ⇒
-              cast(
-                arg,
-                _.toDouble,
-                s"Arg $index of '$arg' not a double " +
-                  s"(or passed string argument isn't matched the corresponding argument in Wasm function"
-              )
-            case _ ⇒
-              Left(
-                InvalidArgError(
-                  s"Invalid type for param $index: $paramType, expected [i32|i64|f32|f64]"
-                )
-              )
-          }
-      }
-
-      result ← list2Either[F, InvalidArgError, AnyRef](args.toList)
-
-    } yield result
-
+    //override def toString: String = functionId.toString
   }
 
 }
