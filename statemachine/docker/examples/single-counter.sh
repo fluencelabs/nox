974e7cfa852d4190e188e43fa153a55bf39a9c9b
Dmitry Sergeev <dmi.sergeev@gmail.com>
2018-10-15 19:24:13 +0500

    minor

--- statemachine/docker/examples/single-counter.sh
+++ statemachine/docker/examples/single-counter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 pushd .
 cd ..
-tendermint init --home="$PWD/nodes/single_node"
-echo "" > "$PWD/nodes/single_node/config/persistent_peers.txt"
-docker run -idt -p 26657:26657 -p 26660:26660 -v "$PWD/statemachine":/statemachine -v "$PWD/examples/vmcode-counter":/vmcode -v "$PWD/nodes/single_node":/tendermint fluencelabs/solver:latest
+tm_home=$HOME/.fluence/nodes/single_node
+tendermint init --home="$tm_home"
+echo "" > "$tm_home/config/persistent_peers.txt"
+docker run -idt -p 26657:26657 -p 26660:26660 -v "$PWD/statemachine":/statemachine -v "$PWD/examples/vmcode-counter":/vmcode -v "$tm_home":/tendermint fluencelabs/solver:latest
 popd
