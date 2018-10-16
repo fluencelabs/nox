238420f115b86188de9c0eaeafe5e7a34b4b0be5
Dmitry Sergeev <dmi.sergeev@gmail.com>
2018-10-16 14:24:51 +0500

    fixes

--- statemachine/docker/examples/sim-cluster-llamadb.sh
+++ statemachine/docker/examples/sim-cluster-llamadb.sh
@@ -3,6 +3,6 @@ pushd . > /dev/null
 cd ..
 ./sim-cluster.sh llamadbnet "$PWD/examples/vmcode-llamadb" 29057 $HOME/.fluence/long-term-keys
 popd > /dev/null
-echo "Connecting to counternet_node3 logs. Ctrl+C to detach.
-echo "Use 'docker logs -f counternet_node3' to reattach"
+echo "Connecting to llamadbnet_node3 logs. Ctrl+C to detach."
+echo "Use 'docker logs -f llamadbnet_node3' to reattach"
 docker logs -f llamadbnet_node3
