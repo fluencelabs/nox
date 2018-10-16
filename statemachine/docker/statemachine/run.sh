ba963c6ec514ec80ffa78cb6292a084e5c17d127
Dmitry Sergeev <dmi.sergeev@gmail.com>
2018-10-16 11:39:15 +0200

    Cluster scripts: git lfs, llamadb launcher, jq erasing, fixes
    * git lfs initialized
    * llamaDB launcher
    * no jq need for running local clusters
    * CheckTx rejected tx logging
    * various fixes

--- statemachine/docker/statemachine/run.sh
+++ statemachine/docker/statemachine/run.sh
@@ -15,12 +15,20 @@ fi
 # copy predefined Tendermint config
 cp -f "$2/config.toml" "$1/config/config.toml"
 
+# extract cluster and info
+node_info_file="$1/config/node_info.json"
+node_index=$(cat "$node_info_file" | jq -r .node_index)
+
+persistent_peers=$(cat "$node_info_file" | jq -r ".cluster|.persistent_peers")
+external_address=$(cat "$node_info_file" | jq -r ".cluster|.external_addrs|.[$node_index]")
+
+cat "$node_info_file" | jq ".cluster|.genesis" > "$1/config/genesis.json"
+
 # set advertised address
-external_address=$(cat "$1/config/external_addr.txt")
 sed -i -e "s#^external_address = \"\"\$#external_address = \"$external_address\"#" "$1/config/config.toml"
 
 # run Tendermint with disabled output
-tendermint node --home="$1" --moniker=$HOSTNAME --p2p.persistent_peers=$(cat $1/config/persistent_peers.txt) > /dev/null 2> /dev/null &
+tendermint node "--home=$1" "--moniker=$HOSTNAME" "--p2p.persistent_peers=$persistent_peers" > /dev/null 2> /dev/null &
 
 # run State machine
 java -Dconfig.file="$2/sm.config" -jar "$3"
