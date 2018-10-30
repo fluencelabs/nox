53561d593d72f6023335c49e8521e5537212cdf0
C.Solovev <constantine.solovev@gmail.com>
2018-10-30 10:33:48 +0400

    [vm] Add unsafe alloc and dealloc for counter example

--- vm/examples/counter/src/main/rust/lib.rs
+++ vm/examples/counter/src/main/rust/lib.rs
@@ -1,6 +1,4 @@
 #![feature(allocator_api)]
-#![feature(extern_prelude)]
-
 #![feature(alloc)]
 extern crate alloc;
 extern crate core;
@@ -22,20 +20,23 @@ pub unsafe fn inc(_ptr: *mut u8, _len: usize) {
 #[no_mangle]
 pub unsafe fn get(_ptr: *mut u8, _len: usize) -> usize {
     fluence::memory::write_str_to_mem(COUNTER.get().to_string())
-        .expect(&format!("[Error] Putting the result string into a raw memory was failed")) as usize
+        .unwrap_or_else(|_| {
+            panic!("[Error] Putting the result string into a raw memory was failed")
+        })
+        .as_ptr() as usize
 }
 
 /// Used from the host environment for memory allocation for passed parameters.
 #[no_mangle]
 pub unsafe fn allocate(size: usize) -> NonNull<u8> {
-    fluence::memory::alloc(size)
-        .expect(&format!("[Error] Allocation of {} bytes failed.", size))
+    fluence::memory::alloc_unsafe(size)
+        .unwrap_or_else(|_| panic!("[Error] Allocation of {} bytes failed.", size))
 }
 
 /// Used from the host environment for memory deallocation after reading results
 /// of function from Wasm memory.
 #[no_mangle]
-pub unsafe fn deallocate(ptr: NonNull<u8>, size: usize) -> () {
-    fluence::memory::dealloc(ptr, size)
-        .expect(&format!("[Error] Deallocate failed for prt={:?} size={}.", ptr, size))
+pub unsafe fn deallocate(ptr: NonNull<u8>, size: usize) {
+    fluence::memory::dealloc_unsafe(ptr, size)
+        .unwrap_or_else(|_| panic!("[Error] Deallocate failed for prt={:?} size={}.", ptr, size))
 }
