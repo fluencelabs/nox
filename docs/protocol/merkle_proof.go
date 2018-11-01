3b715157175887a31a873e6c40f7dd609d26eccb
folex <0xdxdy@gmail.com>
2018-11-01 18:13:50 +0300

    Update code & sync with protocol.md

--- docs/protocol/merkle_proof.go
+++ docs/protocol/merkle_proof.go
@@ -18,8 +18,10 @@ type Node struct {
 
 // Merkle Proof for a range of chunks
 type RangeMerkleProof struct {
-  Chunks []Chunk
-  Hashes [][]Digest
+  Chunks     []Chunk
+  Hashes     [][2]Digest
+  StartChunk int32
+  StopChunk  int32
 }
 
 type ByteRange struct {
@@ -29,22 +31,27 @@ type ByteRange struct {
 }
 
 // Fluence Merkle Tree chunk size
-const FlChunkSize int32 = 6000
+const FlChunkSize int32 = 4000
 
 // Swarm Merkle Tree chunk size
-const SwChunkSize int32 = 4000
+const SwChunkSize int32 = 6000
 
 // split `data` in chunks of size `chunk`
 func Split(data []byte, chunk int32) []Chunk { panic("") }
 
 // calculates the index of the first chunk covering byte range
 func (byteRange ByteRange) StartChunk() int32 {
-  return int32(math.Floor(float64(byteRange.Offset / byteRange.ChunkSize)))
+  return byteRange.Offset / byteRange.ChunkSize
 }
 
 // calculates the index of the last chunk covering byte range
 func (byteRange ByteRange) StopChunk() int32 {
-  return 1 + byteRange.StartChunk() + int32(math.Floor(float64(byteRange.Length/byteRange.ChunkSize)))
+  var stop = byteRange.StartChunk() + byteRange.Length/byteRange.ChunkSize
+  if (byteRange.Length % byteRange.ChunkSize) == int32(0) {
+    stop--
+  }
+
+  return stop
 }
 
 // Merkle Proof for an inclusion of a range of chunks in Fluence Merkle Tree
@@ -71,7 +78,7 @@ func SwRangeMerkleProof(data []byte, offset int32, length int32) RangeMerkleProo
 func BuildMerkleTree(chunks []Chunk, hashFn HashFunc) MerkleTree { panic("") }
 
 // returns Merkle Tree nodes that form a Range Merkle Proof for chunks in the interval [start, stop]
-func RangeProof(tree MerkleTree, start int32, stop int32) [][]Digest { panic("") }
+func RangeProof(tree MerkleTree, start int32, stop int32) [][2]Digest { panic("") }
 
 // builds Merkle Proof for a range of `length` bytes starting at `offset` by splitting them in chunks of size `chunk`
 func BuildRangeMerkleProof(data []byte, byteRange ByteRange, hashFn HashFunc) RangeMerkleProof {
@@ -79,14 +86,16 @@ func BuildRangeMerkleProof(data []byte, byteRange ByteRange, hashFn HashFunc) Ra
   var startChunk = byteRange.StartChunk()
   var stopChunk = byteRange.StopChunk()
 
-  var bottomChunks = chunks[startChunk:stopChunk]
+  var bottomChunks = chunks[startChunk : stopChunk+1]
 
   var tree = BuildMerkleTree(chunks, hashFn)
   var proof = RangeProof(tree, startChunk, stopChunk)
 
   return RangeMerkleProof{
-    Chunks: bottomChunks,
-    Hashes: proof,
+    Chunks:     bottomChunks,
+    Hashes:     proof,
+    StartChunk: startChunk,
+    StopChunk:  stopChunk,
   }
 }
 
