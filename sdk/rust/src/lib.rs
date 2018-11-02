57edc1007802eb6a92fda45a795059b754695abe
Constantine Solovev <constantine.solovev@gmail.com>
2018-11-02 12:50:22 +0400

    Rust sdk (#263)
    * [vm] Draft for Rust Fluence_sdk
    
    * [vm] Counter with SDK
    
    * [vm] Add rustfmt config
    
    * [vm] Add docs, test and refactoring
    
    * [vm] Apply sdk in llamaDb example [tests don't passed]
    
    * [vm] Fix test for llamaDb example
    
    * [vm] Remove linter's warnings
    
    * [vm] Improve Rust docs
    
    * [vm] Return deallocate function
    
    * [vm] Add unsafe alloc and dealloc for counter example
    
    * Adds Travis script (#264)
    
    * [vm] Add Travis script for Rust SDK
    
    * [vm] Review
    
    * Sdk publish (#266)
    
    * [vm] add description to readme
    
    * [vm] Apply Fluence SDK as Rust dependency for vm examples
    
    * [vm] Remove requirements for SDK user to enable #![feature(extern_prelude)] and up SDK versnio to 0.0.3
    
    * [vm] Apply SDK version 0.0.3 for Vm examples
    
    * [vm] Fix typos
    
    * [vm] Avoid useless allocation, improve performance
    
    * [vm] Add checking usize overflow
    
    * [vm] Fix docs
    
    * [vm] A little fixes
    
    * [vm] Remove unsafe alloc and dealloc [llamadb test failed]
    
    * [vm] Up SDK dependencies to 0.0.4
    
    * [vm] Adds tests running for llamadb VM example
    
    * [vm] Add html_root_url
    
    * [vm] Fix (signal: 11, SIGSEGV: invalid memory reference) in llamadb tests.

---
+++ sdk/rust/src/lib.rs
@@ -0,0 +1,7 @@
+//! Rust SDK for writing applications for Fluence.
+#![doc(html_root_url = "https://docs.rs/fluence_sdk/0.0.4")]
+#![feature(allocator_api)]
+
+extern crate core;
+
+pub mod memory;
---
+++ sdk/rust/src/memory/errors.rs
@@ -0,0 +1,62 @@
+//! Contains definitions for memory errors.
+
+use core::alloc::LayoutErr;
+use std::alloc::AllocErr;
+use std::error::Error;
+use std::fmt;
+use std::fmt::Display;
+use std::io;
+
+#[derive(Debug, Clone)]
+pub struct MemError(String);
+
+impl Display for MemError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "MemError({:?})", self)
+    }
+}
+
+impl Error for MemError {}
+
+impl MemError {
+    pub fn new(message: &str) -> Self {
+        MemError(message.to_string())
+    }
+
+    pub fn from_err<E: Error + Display>(err: &E) -> Self {
+        MemError::new(&err.to_string())
+    }
+}
+
+/// Creates From instance for MemError for each specified error types.
+///
+/// Example:
+///
+/// ```
+/// // usage of macro:
+///
+/// mem_error_from![io::Error]
+///
+/// // code will be generated:
+///
+/// impl From<io::Error> for MemError {
+///    fn from(err: io::Error) -> Self {
+///        MemError::from_err(err)
+///    }
+/// }
+///
+/// ```
+// todo move this macro to utils or use 'quick-error' or 'error-chain' crates
+macro_rules! mem_error_from {
+    ( $( $x:ty );* ) => {
+        $(
+            impl From<$x> for MemError {
+                fn from(err: $x) -> Self {
+                    MemError::from_err(&err)
+                }
+            }
+        )*
+    }
+}
+
+mem_error_from! [LayoutErr; AllocErr; io::Error];
