b0ec35705b63d9ea3361c9dc8499f7dca47260af
vms <michail.vms@gmail.com>
2018-11-22 17:12:33 +0300

    the initial bench version

---
+++ vm/vm_bench/fib_recursive/src/lib.rs
@@ -0,0 +1,20 @@
+const FIB_NUMBER : i64 = 40;
+const ITERATION_COUNT : i32 = 100;
+
+#[no_mangle]
+pub extern "C" fn fib(num: i64) -> i64 {
+	if num == 1 || num == 2 {
+		return 1
+	}
+
+	fib(num - 1) * fib(num - 2)
+}
+
+#[no_mangle]
+pub extern "C" fn test() -> i64 {
+	for _ in 1..ITERATION_COUNT {
+		fib(FIB_NUMBER);
+	}
+
+	fib(FIB_NUMBER)
+}
