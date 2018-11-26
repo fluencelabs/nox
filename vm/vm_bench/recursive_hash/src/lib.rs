264c18457634c8932ea67fcfd52a2dd07d69c6d1
vms <michail.vms@yandex.ru>
2018-11-26 10:31:37 +0300

    update recursive hash

--- vm/vm_bench/recursive_hash/src/lib.rs
+++ vm/vm_bench/recursive_hash/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod settings;
+
+use settings::{INITIAL_VALUE, ITERATIONS_COUNT};
+use sha3::{Digest, Sha3_256};
+
+#[no_mangle]
+pub fn bench_test() {
+    let mut hasher = Sha3_256::new();
+    hasher.input(b"asd");
+
+//    for _ in 1..ITERATIONS_COUNT {
+//        result = hasher.input(result);
+//    }
+
+    let result = hasher.result();
 }
---
+++ vm/vm_bench/recursive_hash/src/settings.rs
@@ -0,0 +1,2 @@
+pub const ITERATIONS_COUNT: i64 = 1000;
+pub const INITIAL_VALUE: i64 = 0;
