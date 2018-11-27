e10c04c69c31ad50edd3a79552f4ed4366f65122
vms <michail.vms@gmail.com>
2018-11-27 19:49:57 +0300

    add deflate test

---
+++ vm/vm_bench/deflate_compression/src/settings.rs
@@ -0,0 +1,8 @@
+// this values is used as a seed to pseudo-random generator for sequence generation
+pub const SEED : &'static str = env!("SEED");
+
+// count of test iterations
+pub const ITERATIONS_COUNT: &'static str = env!("ITERATIONS_COUNT");
+
+// size of sequence that should be compressed in each iteration
+pub const SEQUENCE_SIZE: &'static str = env!("SEQUENCE_SIZE");
