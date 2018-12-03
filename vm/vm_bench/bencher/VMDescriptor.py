ab210ae1a9c0dd3222efbb62653f7a4de6094387
vms <michail.vms@gmail.com>
2018-12-03 23:41:46 +0300

    delete bencher and results

--- vm/vm_bench/bencher/VMDescriptor.py
+++
@@ -1,5 +0,0 @@
-class VMDescriptor:
-    def __init__(self, relative_vm_binary_path="", arg_str="", is_compiler_type=True):
-        self.relative_vm_binary_path = relative_vm_binary_path
-        self.arg_str = arg_str
-        self.is_compiler_type = is_compiler_type
