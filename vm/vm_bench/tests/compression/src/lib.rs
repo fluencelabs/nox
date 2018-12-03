3c4a08a3f6ab610040f972d7138cd19c7fd59b08
vms <michail.vms@gmail.com>
2018-12-03 18:50:05 +0300

    move toone compression file for test with defalte and snappy compressions

--- vm/vm_bench/tests/compression/src/lib.rs
+++ vm/vm_bench/tests/compression/src/lib.rs
@@ -1,12 +1,16 @@
+
+
 mod settings;
 
 extern crate snap;
+extern crate deflate;
 extern crate rand;
 extern crate rand_isaac;
 
 use settings::{SEED, ITERATIONS_COUNT, SEQUENCE_SIZE};
 use rand::{Rng, SeedableRng};
 use rand_isaac::IsaacRng;
+use deflate::deflate_bytes;
 
 type Sequence = Vec<u8>;
 
@@ -20,30 +24,28 @@ fn generate_sequence(seed : u64, size : u64) -> Sequence {
     result_sequence
 }
 
-fn bench_test() -> u64 {
-    let seed : u64 = SEED.parse::<u64>().unwrap();
-    let iterations_count : u64 = ITERATIONS_COUNT.parse::<u64>().unwrap();
-    let sequence_size : u64 = SEQUENCE_SIZE.parse::<u64>().unwrap();
-
-    let mut sequence = generate_sequence(seed, sequence_size);
-    let mut compressed_sequence = snap::Encoder::new().compress_vec(&sequence).unwrap();
-
-    for _ in 1..iterations_count {
-        let new_seed = compressed_sequence.len() +
-            compressed_sequence.iter().fold(0u8, |x1, x2| x1 ^ x2) as usize;
-        sequence = generate_sequence(new_seed as u64, sequence_size);
-        compressed_sequence = snap::Encoder::new().compress_vec(&sequence).unwrap();
+fn compress_sequence(sequence: &Sequence) -> Sequence {
+    if cfg!(feature = "deflate_compression") {
+        return deflate_bytes(&sequence);
     }
 
-    compressed_sequence.iter().fold(0u8, |x1, x2| x1 ^ x2) as u64
-}
-
-#[no_mangle]
-pub extern "C" fn app_main() -> u64 {
-    bench_test()
+    return snap::Encoder::new().compress_vec(&sequence).unwrap();
 }
 
 #[no_mangle]
 pub extern "C" fn main() -> u64 {
-    bench_test()
+    let seed : u64 = SEED.parse::<u64>().unwrap();
+    let iterations_count : u64 = ITERATIONS_COUNT.parse::<u64>().unwrap();
+    let sequence_size : u64 = SEQUENCE_SIZE.parse::<u64>().unwrap();
+
+    let mut compressed_sequence = generate_sequence(seed, sequence_size);
+
+    for _ in 1..iterations_count {
+        let new_seed = compressed_sequence.len() +
+            compressed_sequence.iter().fold(0u8, |x1, x2| x1 ^ x2) as usize;
+        compressed_sequence = generate_sequence(new_seed as u64, sequence_size);
+        compressed_sequence = compress_sequence(&compressed_sequence);
+    }
+
+    compressed_sequence.iter().fold(0u8, |x1, x2| x1 ^ x2) as u64
 }
