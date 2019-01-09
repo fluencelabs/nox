c56abe16ef8a9b5d198282eefa45aa5de377cbc6
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-01-09 14:22:36 +0300

    comments

--- tools/deploy/scripts/deploy-contract.js
+++ tools/deploy/scripts/deploy-contract.js
@@ -1,12 +1,15 @@
 let fs = require("fs");
 let Web3 = require('web3');
 
+// Connect to the local ethereum node
 let web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
+// Get ABI from compiled contract
 let source = fs.readFileSync("../../../bootstrap/contracts/compiled/Network.abi");
 let abi = JSON.parse(source);
 
+// Get bytecode from compiled contract
 let sourceBin = fs.readFileSync("../../../bootstrap/contracts/compiled/Network.bin");
 let bytecode = '0x' + sourceBin;
 
@@ -20,11 +23,12 @@ let acc = '0x00a329c0648769a73afac7f9381e08fb43dbea72'
 var password = "";
 web3.eth.personal.unlockAccount(acc, password)
 
-
+// Get encoded request for deploying contract
 let encodedAbi = NetworkContract.deploy({
     data: bytecode
 }).encodeABI();
 
+// Send transaction and print the result
 web3.eth.sendTransaction({ from: acc, data: encodedAbi, gas: '4500000' }, "")
 .on('error', console.error)
 .then(function(newContractInstance){
