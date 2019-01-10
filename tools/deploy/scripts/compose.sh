5bb7ad2ca0f8e65094a4e8d6b9b7a50c3d5722b0
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-01-10 11:00:03 +0300

    change args for parity

--- tools/deploy/scripts/compose.sh
+++ tools/deploy/scripts/compose.sh
@@ -10,7 +10,7 @@ then
     export PRIVATE_KEY=4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7
     export PARITY_ARGS='--config dev --jsonrpc-apis=all --jsonrpc-hosts=all --jsonrpc-cors="*" --unsafe-expose'
 else
-    export PARITY_ARGS='--light --chain kovan --jsonrpc-apis=all --jsonrpc-hosts all --jsonrpc-cors "*" --unsafe-expose'
+    export PARITY_ARGS='--light --chain kovan --jsonrpc-apis=all --jsonrpc-hosts=all --jsonrpc-cors="*" --unsafe-expose'
 
 fi
 
