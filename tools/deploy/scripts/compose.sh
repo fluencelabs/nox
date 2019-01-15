4dc8c46e6fb855b4c89ec6466689fff0b7f3486f
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-01-15 15:08:01 +0300

    fix headers

--- tools/deploy/scripts/compose.sh
+++ tools/deploy/scripts/compose.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-#
 # Copyright 2018 Fluence Labs Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+#!/bin/bash
 
 set -e
 
