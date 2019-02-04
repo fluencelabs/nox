ca734fe457980a36766dfd1e44b48846330ab8a1
folex <0xdxdy@gmail.com>
2019-02-04 10:06:05 +0300

    Update truffle.js

--- bootstrap/truffle.js
+++ bootstrap/truffle.js
@@ -26,20 +26,3 @@ module.exports = {
     }
   }
 };
-
-// module.exports = {
-//   compilers: {
-//     solc: {
-//       version: <string>, // A version or constraint - Ex. "^0.5.0"
-//                          // Can also be set to "native" to use a native solc
-//       docker: <boolean>, // Use a version obtained through docker
-//       settings: {
-//         optimizer: {
-//           enabled: <boolean>,
-//           runs: <number>   // Optimize for how many times you intend to run the code
-//         }
-//         evmVersion: <string> // Default: "byzantium"
-//       }
-//     }
-//   }
-// }
