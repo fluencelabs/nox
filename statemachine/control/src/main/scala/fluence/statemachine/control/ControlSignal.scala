0bf09b06dc2f031e9abd34d0690cfda2eb3baa8e
folex <0xdxdy@gmail.com>
2019-02-06 11:05:38 +0300

    Docs

--- statemachine/control/src/main/scala/fluence/statemachine/control/ControlSignal.scala
+++ statemachine/control/src/main/scala/fluence/statemachine/control/ControlSignal.scala
@@ -6,9 +6,15 @@ import scodec.bits.ByteVector
 // Common trait for all control signals from node to worker
 sealed trait ControlSignal
 
-// A signal to change a voting power of the specified Tendermint validator. Voting power zero votes to remove.
-// Represents a Tendermint's ValidatorUpdate command
-// see https://github.com/tendermint/tendermint/blob/master/docs/spec/abci/abci.md#validatorupdate
+/**
+ * A signal to change a voting power of the specified Tendermint validator. Voting power zero votes to remove.
+ * Represents a Tendermint's ValidatorUpdate command
+ * see https://github.com/tendermint/tendermint/blob/master/docs/spec/abci/abci.md#validatorupdate
+ *
+ * @param keyType Type of a Tendermint node's validator key. Currently always PubKeyEd25519.
+ * @param validatorKey Validator key's bytes
+ * @param votePower Desired resulting vote power of the validator. Zero votes to remove that node from validators set.
+ */
 case class ChangePeer(keyType: String, validatorKey: ByteVector, votePower: Long) extends ControlSignal
 
 object ChangePeer {
