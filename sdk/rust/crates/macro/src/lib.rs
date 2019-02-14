7d1ce5c0ec1186fda64b4753de754d3cb1c5e012
vms <michail.vms@gmail.com>
2019-02-14 14:51:07 +0300

    prepare to publish

--- sdk/rust/crates/macro/src/lib.rs
+++ sdk/rust/crates/macro/src/lib.rs
@@ -78,7 +78,7 @@
 //! Please find more examples in `https://github.com/fluencelabs/fluence/tree/master/vm/examples`.
 //!
 
-#![doc(html_root_url = "https://docs.rs/fluence-sdk-macro/0.0.9")]
+#![doc(html_root_url = "https://docs.rs/fluence-sdk-macro/0.0.10")]
 
 extern crate proc_macro;
 mod macro_attr_parser;
