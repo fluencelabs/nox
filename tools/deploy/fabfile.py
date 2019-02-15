8aed725c94addef9234f6352339ba56150ea6ca5
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-02-15 11:10:10 +0300

    add base path, use root user for parity container

--- tools/deploy/fabfile.py
+++ tools/deploy/fabfile.py
@@ -106,6 +106,8 @@ def deploy():
                        CONTRACT_ADDRESS=contract_address,
                        OWNER_ADDRESS=current_owner,
                        PORTS=current_ports,
+                       PARITY_RESERVED_PEERS="../config/reserved_peers.txt",
+                       PARITY_STORAGE="~/.parity",
                        # container name
                        NAME="fluence-node-1",
                        HOST_IP=current_host):
