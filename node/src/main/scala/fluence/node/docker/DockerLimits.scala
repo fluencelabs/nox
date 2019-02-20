3822c9616e1975d52a13106ef962a087c53c2ca6
folex <0xdxdy@gmail.com>
2019-02-20 16:58:55 +0300

    Add enc / dev for DockerLimits

--- node/src/main/scala/fluence/node/docker/DockerLimits.scala
+++ node/src/main/scala/fluence/node/docker/DockerLimits.scala
@@ -1,11 +1,18 @@
 package fluence.node.docker
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Decoder, Encoder}
 
 /**
  * Limits on cpu and memory of a container
- * @see [[DockerParams.cpus]], [[DockerParams.memory]], [[DockerParams.memoryReservation]]
  *
+ * @see [[DockerParams.cpus]], [[DockerParams.memory]], [[DockerParams.memoryReservation]]
  * @param cpus Fraction number of max cores available to a container.
  * @param memoryMb A hard limit on maximum amount of memory available to a container
  * @param memoryReservationMb Amount of memory guaranteed to be allocated for a container
  */
 case class DockerLimits(cpus: Option[Double], memoryMb: Option[Int], memoryReservationMb: Option[Int])
+
+object DockerLimits {
+  implicit val enc: Encoder[DockerLimits] = deriveEncoder
+  implicit val dec: Decoder[DockerLimits] = deriveDecoder
+}
