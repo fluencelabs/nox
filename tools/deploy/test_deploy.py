c45d335317a5c641aa9958194c14524c2468a6b4
folex <0xdxdy@gmail.com>
2019-02-25 12:04:26 +0300

    Support capacity and api port in CLI, deployment scripts and docs (#488)

--- tools/deploy/test_deploy.py
+++ tools/deploy/test_deploy.py
@@ -9,13 +9,13 @@ def test_json_format():
     env_with_args["TENDERMINT_NODE_ID"] = "qwe"
     env_with_args["CONTRACT_ADDRESS"] = "ca"
     env_with_args["OWNER_ADDRESS"] = "oa"
-    env_with_args["START_PORT"] = "25000"
-    env_with_args["LAST_PORT"] = "25100"
+    env_with_args["API_PORT"] = "25000"
+    env_with_args["CAPACITY"] = "111"
 
     output = subprocess.check_output(['bash', 'scripts/compose.sh', 'generate_json'], env=env_with_args)
     json_data = json.loads(output)
     register_command = utils.register_command(json_data, "some_key")
 
-    checked_result = "./fluence register --node_ip 123 --tendermint_key 456 --tendermint_node_id qwe --contract_address ca --account oa --secret_key some_key --start_port 25000 --last_port 25100 --eth_url http://123:8545 --wait_syncing --gas_price 10 --base64_tendermint_key"
+    checked_result = "./fluence register --node_ip 123 --tendermint_key 456 --tendermint_node_id qwe --contract_address ca --account oa --secret_key some_key --api_port 25000 --capacity 111 --eth_url http://123:8545 --wait_syncing --gas_price 10 --base64_tendermint_key"
 
     assert register_command == checked_result
