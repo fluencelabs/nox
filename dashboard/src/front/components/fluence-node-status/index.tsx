0440c3c3f6dd38237dbff9d086aea5e70df0e754
Igor Krivchun <igor@krivchun.ru>
2019-02-27 01:27:27 +0700

    Add dashboard styling (#495)

--- dashboard/src/front/components/fluence-node-status/index.tsx
+++ dashboard/src/front/components/fluence-node-status/index.tsx
@@ -28,28 +28,59 @@ class FluenceNodeStatus extends React.Component<Props, State> {
         this.props.retrieveNodeStatus(this.props.node).then(this.props.hideLoading).catch(this.props.hideLoading);
     }
 
+    renderNodeStatusInfo(nodeInfo: NodeInfo): React.ReactNode {
+        let statusInfo = null;
+        if (nodeInfo.status) {
+            statusInfo = (
+                <div className="box-body">
+                    <strong><i className="fa fa-bullseye margin-r-5"></i>Ip Address</strong>
+                    <p className="text-muted">{nodeInfo.status.ip}</p>
+                    <hr/>
+
+                    <strong><i className="fa fa-bullseye margin-r-5"></i>List Of Ports:</strong>
+                    <p className="text-muted">{nodeInfo.status.listOfPorts}</p>
+                    <hr/>
+
+                    <strong><i className="fa fa-bullseye margin-r-5"></i>Uptime:</strong>
+                    <p className="text-muted">{nodeInfo.status.uptime}</p>
+                    <hr/>
+
+                    <strong><i className="fa fa-bullseye margin-r-5"></i>Workers count:</strong>
+                    <p className="text-muted">{nodeInfo.status.numberOfWorkers}</p>
+                </div>
+            );
+        } else if (nodeInfo.causeBy) {
+            statusInfo = (
+                <div className="box-body">
+                    <strong><i className="fa fa-bullseye margin-r-5"></i>Error</strong>
+                    <p className="text-muted">{nodeInfo.causeBy.toString()}</p>
+
+                </div>
+            );
+        }
+
+        return (
+            <div className="box-footer no-padding">
+                {statusInfo}
+            </div>
+        );
+    }
+
     render(): React.ReactNode {
         const nodeInfo = this.props.nodesStatus[this.props.node.id];
-        let nodeStatus = null;
-        if (nodeInfo && nodeInfo.status) {
-            nodeStatus = (
-                <ul>
-                    <li>id: {this.props.node.id}</li>
-                    <li>ip: {nodeInfo.status.ip}</li>
-                    <li>listOfPorts: {nodeInfo.status.listOfPorts}</li>
-                    <li>uptime: {nodeInfo.status.uptime}</li>
-                    <li>numberOfWorkers: {nodeInfo.status.numberOfWorkers}</li>
-                </ul>
-            );
-        } else if (nodeInfo && nodeInfo.causeBy) {
-            nodeStatus = (<span>Error: {nodeInfo.causeBy}</span>);
-        } else {
-            nodeStatus = (<span>Loading...</span>);
-        }
+
         return (
-            <div>
-                <h3>Node status {this.props.node.id}</h3>
-                {nodeStatus}
+            <div className="col-md-4">
+                <div className="box box-widget widget-user-2">
+                    <div className="widget-user-header bg-green">
+                        <div className="widget-user-image">
+                            <span className="entity-info-box-icon"><i className={nodeInfo ? 'ion ion-android-laptop' : 'fa fa-refresh fa-spin'}></i></span>
+                        </div>
+                        <h3 className="widget-user-username">Node Status</h3>
+                        <h5 className="widget-user-desc">ID:&nbsp;{this.props.node.id}</h5>
+                    </div>
+                    { nodeInfo && this.renderNodeStatusInfo(nodeInfo) }
+                </div>
             </div>
         );
     }
