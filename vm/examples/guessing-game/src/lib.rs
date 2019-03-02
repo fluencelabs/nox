824581fafca380c38a899bd502361361912726c6
folex <0xdxdy@gmail.com>
2019-03-02 20:03:55 +0300

    Implement guessing game

--- vm/examples/guessing-game/src/lib.rs
+++ vm/examples/guessing-game/src/lib.rs
@@ -16,45 +16,114 @@
 
 //! A simple demo application for Fluence.
 use fluence::sdk::*;
-use log::info;
-use rand::Rng;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
-use rand::{Rng, SeedableRng, StdRng};
+use rand::{Rng, SeedableRng};
+use rand_isaac::IsaacRng;
+use std::cell::RefCell;
 
-
-
-thread_local! {
-//    static GAME_MANAGER: RefCell<GameManager> = RefCell::new(GameManager::new());
-    static mut GAMES_COUNT: u32 = 0;
-    static mut SEED: u64 = 123456789;
-    static mut SECRET: u8 = 0;
+enum Guess {
+    LESS,
+    GREATER,
+    EQUAL
 }
 
+thread_local! {
+    static GAMES_COUNT: RefCell<u32> = RefCell::new(0);
+    static TRIES: RefCell<u32> = RefCell::new(0);
+    static SEED: RefCell<u64> = RefCell::new(0);
+    static SECRET: RefCell<u8> = RefCell::new(0);
+}
 
 fn init() {
-    logger::WasmLogger::init_with_level(log::Level::Info).unwrap();
+    SEED.with(|seed| *seed.borrow_mut() = 123456789);
 }
 
 #[invocation_handler(init_fn = init)]
 fn game(input: String) -> String {
+    update_state(&input);
+
+    match input.parse::<i16>() {
+        Err(e) => format!("Input can't be parsed as i16 {}: {}", input, e),
+        Ok(guess) => match check_guess(guess) {
+            Guess::LESS => format!("Your guess is too low! Try something bigger."),
+            Guess::GREATER => format!("Too big! Try again."),
+            Guess::EQUAL => {
+                next_game();
+                format!("Success! You guessed right.")
+            }
+        }
+    }
+}
+
+fn next_game() {
+    GAMES_COUNT.with(|c| *c.borrow_mut() += 1);
+    update_secret();
+}
+
+fn update_state(input: &String) {
+    TRIES.with(|t| *t.borrow_mut() += 1);
+
     update_seed(input);
+
+    SECRET.with(|secret| {
+        if *secret.borrow() == 0 {
+            update_secret()
+        }
+    })
 }
 
-fn update_seed(input: String) {
-    let mut hasher = DefaultHasher::new();
-    hasher.write(input.as_bytes());
-    hasher.write_u32(GAMES_COUNT);
-    hasher.write(SEED);
-    SEED = hasher.finish();
+fn update_seed(input: &String) {
+    GAMES_COUNT.with(|count| SEED.with(|seed| {
+        let count = *count.borrow();
+        let mut seed = seed.borrow_mut();
+        let mut hasher = DefaultHasher::new();
+        hasher.write(input.as_bytes());
+        hasher.write_u32(count);
+        hasher.write_u64(*seed);
+        *seed = hasher.finish();
+    }))
 }
 
-fn gen_secret() -> u8 {
-    let mut rng: StdRng = SeedableRng::from_seed(SEED);
+fn update_secret() {
+    SEED.with(|seed| {
+        let mut rng: IsaacRng = SeedableRng::seed_from_u64(*seed.borrow());
+        SECRET.with(|secret|{
+            *secret.borrow_mut() = rng.gen::<u8>();
+        })
+    })
 }
 
-fn greeting(name: String) -> String {
-    let answer = format!("Hello, {}! Please, input your guess");
-    info!(answer);
-    answer
+fn check_guess(guess: i16) -> Guess {
+    SECRET.with(|secret| {
+        let secret = *secret.borrow() as i16;
+        match guess {
+            _ if guess < secret => Guess::LESS,
+            _ if guess > secret => Guess::GREATER,
+            _ => Guess::EQUAL
+        }
+    })
+}
+
+#[test]
+fn test_low() {
+    let expected_answer = String::from("Your guess is too low! Try something bigger.");
+    let answer = game("-1".to_string());
+    assert_eq!(answer, expected_answer);
+}
+
+#[test]
+fn test_high() {
+    let expected_answer = String::from("Too big! Try again.");
+    let answer = game(i16::max_value().to_string());
+    assert_eq!(answer, expected_answer);
+}
+
+#[test]
+fn test_eq() {
+    let value: u8 = 10;
+    SECRET.with(|secret| *secret.borrow_mut() = value);
+    let expected_answer = String::from("Success! You guessed right.");
+    let answer = game(value.to_string());
+    assert_eq!(answer, expected_answer);
 }
