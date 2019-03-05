388ba152b870459322592dd828591a3e719b864b
Igor Krivchun <igor@krivchun.ru>
2019-03-05 16:37:55 +0700

    Fix dasboard UX and styling (#527)

--- dashboard/src/fluence/contract.ts
+++ dashboard/src/fluence/contract.ts
@@ -1,7 +1,7 @@
 import { getContract } from './';
 import Web3 from "web3";
 import { Network } from "../../types/web3-contracts/Network";
-import { defaultContractAddress, rootTagId } from '../constants';
+import { defaultContractAddress, fluenceNodeAddr, rootTagId } from '../constants';
 
 const search = (window as any).location.search;
 const urlParams = new URLSearchParams(search);
@@ -12,8 +12,7 @@ const contractFromTag = rootElement ? rootElement.getAttribute('data-contract') 
 
 export const contractAddress: string = contractFromUrl ? contractFromUrl : (contractFromTag ? contractFromTag : defaultContractAddress);
 
-const web3 = (window as any).web3;
-const web3js = new Web3(web3.currentProvider);
+const web3js = new Web3(new Web3.providers.HttpProvider(fluenceNodeAddr));
 const contract = getContract(contractAddress, web3js);
 
 export default contract as Network;
