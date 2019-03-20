bf5eda93c1fc95ad24514d26d8755ef31bd94f7b
folex <0xdxdy@gmail.com>
2019-03-20 12:01:58 +0300

    wip

--- vm/src/it/resources/test-cases/llamadb/src/lib.rs
+++ vm/src/it/resources/test-cases/llamadb/src/lib.rs
@@ -26,11 +26,20 @@ mod tests;
 #[macro_use]
 extern crate lazy_static;
 
+use core::fmt;
 use llamadb::tempdb::{ExecuteStatementResponse, TempDb};
+use secp256k1::PublicKey;
 use std::error::Error;
 use std::sync::Mutex;
-use secp256k1::{Secp256k1, Message, SecretKey, PublicKey};
 
+#[derive(Debug)]
+struct MyError(String);
+impl Error for MyError {}
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 /// Result for all possible Error types.
 type GenResult<T> = ::std::result::Result<T, Box<Error>>;
@@ -42,28 +51,48 @@ lazy_static! {
 
 // 64 + 1 byte prefix, should it be just 64?
 static PUBLIC_KEY: [u8; 65] = [
-    0x04, 0xfb, 0x6e, 0x27, 0x79, 0x77, 0xf4, 0x67,
-    0x61, 0x8a, 0xde, 0x83, 0xf7, 0x50, 0x5b, 0x6f,
-    0x44, 0x8b, 0xed, 0x40, 0xff, 0x10, 0x6d, 0xfd,
-    0xde, 0x56, 0xde, 0x82, 0xfb, 0x14, 0xc7, 0x8a,
-    0x53, 0x07, 0x57, 0x8e, 0x60, 0x91, 0x90, 0xd6,
-    0x5f, 0xc6, 0x39, 0x61, 0x97, 0x0c, 0xf1, 0x48,
-    0x62, 0x3f, 0x3d, 0xc8, 0xfc, 0x8e, 0x33, 0x17,
-    0x7a, 0xa0, 0x5a, 0xdb, 0x4b, 0x78, 0x10, 0x28, 0x2b
+    0x04, 0xfb, 0x6e, 0x27, 0x79, 0x77, 0xf4, 0x67, 0x61, 0x8a, 0xde, 0x83, 0xf7, 0x50, 0x5b, 0x6f,
+    0x44, 0x8b, 0xed, 0x40, 0xff, 0x10, 0x6d, 0xfd, 0xde, 0x56, 0xde, 0x82, 0xfb, 0x14, 0xc7, 0x8a,
+    0x53, 0x07, 0x57, 0x8e, 0x60, 0x91, 0x90, 0xd6, 0x5f, 0xc6, 0x39, 0x61, 0x97, 0x0c, 0xf1, 0x48,
+    0x62, 0x3f, 0x3d, 0xc8, 0xfc, 0x8e, 0x33, 0x17, 0x7a, 0xa0, 0x5a, 0xdb, 0x4b, 0x78, 0x10, 0x28,
+    0x2b,
 ];
 
 fn get_pk() -> PublicKey {
-    PublicKey::from_slice(&PUBLIC_KEY).expect("Invalid public key")
+    PublicKey::parse_slice(&PUBLIC_KEY, None).expect("Invalid public key")
+}
+
+fn err_msg(s: &str) -> Box<Error> {
+    MyError(s.to_string()).into()
 }
 
 fn check_signature(signature: &str) -> bool {
     false
 }
 
+fn split(input: &String) -> GenResult<(&str, &str)> {
+    let pos: usize = input.find("\n").ok_or(err_msg(
+        "Invalid input. Should be <signature>\\n<sql_query>",
+    ))?;
+    let signature: &str = &input[..pos];
+    let sql_str: &str = &input[pos + 1..];
+    Ok((signature, sql_str))
+}
+
+fn check_input(input: &String) -> GenResult<&str> {
+    let (signature, sql_str) = split(input)?;
+    if check_signature(signature) {
+        Ok(sql_str)
+    } else {
+        Err(err_msg("Invalid signature"))
+    }
+}
+
 /// Executes SQL and converts llamadb error to string.
 #[invocation_handler]
-fn main(sql_str: String) -> String {
-    match run_query(&sql_str) {
+fn main(input: String) -> String {
+    let result = check_input(&input).and_then(run_query);
+    match result {
         Ok(response) => response,
         Err(err_msg) => format!("[Error] {}", err_msg),
     }
@@ -82,7 +111,9 @@ fn statement_to_string(statement: ExecuteStatementResponse) -> String {
     match statement {
         ExecuteStatementResponse::Created => "table created".to_string(),
         ExecuteStatementResponse::Dropped => "table was dropped".to_string(),
-        ExecuteStatementResponse::Inserted(number) => format!("rows inserted: {} {:?}", number, *PK),
+        ExecuteStatementResponse::Inserted(number) => {
+            format!("rows inserted: {} {:?}", number, *PK)
+        }
         ExecuteStatementResponse::Select { column_names, rows } => {
             let col_names = column_names.to_vec().join(", ") + "\n";
             let rows_as_str = rows
