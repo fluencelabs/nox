e6a49fecc02481c46c061000340e3f5391856a9e
Igor Krivchun <igor@krivchun.ru>
2019-03-25 02:10:16 +0700

    Dashboard deploy app improvement

--- dashboard/src/front/actions/deployable/deploy.ts
+++ dashboard/src/front/actions/deployable/deploy.ts
@@ -1,21 +1,27 @@
 import contract from '../../../fluence/contract';
-import {checkLogs, DeployableApp, send, txParams} from "../../../fluence/deployable";
+import {checkLogs, DeployableApp, send, txParams, deployableApps} from "../../../fluence/deployable";
 import {privateKey} from "../../../constants";
 import {Action, Dispatch} from "redux";
-
+import Cookies from 'js-cookie';
 import EthereumTx from "ethereumjs-tx";
 
+export const DEPLOY_STATE_PREPARE = 'DEPLOY_STATE_PREPARE';
+export const DEPLOY_STATE_TRX = 'DEPLOY_STATE_TRX';
 export const DEPLOY_TX_REVERTED = 'DEPLOY_TX_REVERTED';
 export const APP_DEPLOYED = 'APP_DEPLOYED';
 export const APP_ENQUEUED = 'APP_ENQUEUED';
 export const APP_DEPLOY_FAILED = 'APP_DEPLOY_FAILED';
 
-export const deploy = (app: DeployableApp) => {
+export const deploy = (app: DeployableApp, appTypeId: string) => {
     return async (dispatch: Dispatch): Promise<Action> => {
+
+        dispatch({type: DEPLOY_STATE_PREPARE});
+
         let txData = contract.methods.addApp(app.storageHash, "0x0", app.clusterSize, []).encodeABI();
         let tx = new EthereumTx(await txParams(txData));
         tx.sign(privateKey);
 
+        dispatch({type: DEPLOY_STATE_TRX});
         let receipt = await send(tx.serialize());
 
         if (!receipt.status) {
@@ -24,11 +30,65 @@ export const deploy = (app: DeployableApp) => {
 
         let [type, appId] = checkLogs(receipt);
 
+        Cookies.remove('deployedAppId');
+        Cookies.remove('deployedAppTypeId');
+        Cookies.set('deployedAppId', String(appId), { expires: 365 });
+        Cookies.set('deployedAppTypeId', String(appTypeId), { expires: 365 });
+
         return dispatch({
             type: type,
             appId: appId,
-            app: app
+            app: app,
+            trxHash: receipt.transactionHash
         });
     };
 };
 
+export const DEPLOY_RESTORE = 'DEPLOY_RESTORE';
+export const restoreDeployed = (appId: string, appTypeId: string) => {
+    return (dispatch: Dispatch): Action => {
+        return dispatch({
+            type: DEPLOY_RESTORE,
+            appId,
+            app: deployableApps[appTypeId],
+        });
+    };
+};
+
+export default (state = {}, action: any) => {
+    switch (action.type) {
+        case DEPLOY_STATE_PREPARE: {
+            return {
+                ...state,
+                deployState: 'prepare'
+            };
+        }
+        case DEPLOY_STATE_TRX: {
+            return {
+                ...state,
+                deployState: 'trx'
+            };
+        }
+        case APP_DEPLOYED:
+        case APP_ENQUEUED: {
+            return {
+                ...state,
+                deployState: 'end',
+                app: action.app,
+                appId: action.appId,
+                trxHash: action.trxHash
+            };
+        }
+        case DEPLOY_RESTORE: {
+            return {
+                ...state,
+                deployState: 'end',
+                app: action.app,
+                appId: action.appId,
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+};
--- dashboard/src/front/components/fluence-deployable-app/snippets.tsx
+++ dashboard/src/front/components/fluence-deployable-app/snippets.tsx
@@ -2,26 +2,69 @@ import * as React from 'react';
 import {connect} from 'react-redux';
 import {DeployableApp} from "../../../fluence/deployable";
 import {defaultContractAddress} from "../../../constants";
+import {
+    displayLoading,
+    hideLoading,
+    retrieveApp,
+} from '../../actions';
+import FluenceCluster from '../fluence-cluster';
+import {App, AppId} from "../../../fluence";
+import {Action} from "redux";
+import {cutId} from "../../../utils";
 
 interface State {
 }
 
 interface Props {
+    deployState: string | undefined,
     appId: number | undefined,
     app: DeployableApp | undefined,
+    apps: {
+        [key: string]: App
+    },
+    trxHash: string,
+    retrieveApp: (appId: AppId) => Promise<Action>,
+    displayLoading: typeof displayLoading,
+    hideLoading: typeof hideLoading,
 }
 
 class Snippets extends React.Component<Props, State> {
     state: State = {};
 
+    loadData(): void {
+        this.props.displayLoading();
+        this.props.retrieveApp(String(this.props.appId)).then(this.props.hideLoading).catch(this.props.hideLoading);
+    }
+
+    componentDidUpdate(prevProps: Props): void {
+        if (this.props.appId && prevProps.appId != this.props.appId) {
+            this.loadData();
+        }
+    }
+
+    getDeployStateLabel(deployState: string|undefined): string {
+        switch (deployState) {
+            case 'prepare': {
+                return 'preparing transaction...';
+            }
+            case 'trx': {
+                return 'sending transaction...';
+            }
+            default: {
+                return '';
+            }
+        }
+    }
+
     render(): React.ReactNode {
         if (this.props.app != undefined && this.props.appId != undefined) {
+            const appInfo = this.props.apps[this.props.appId];
             return (
                 <div className="box box-widget widget-user-2">
                     <div className="widget-user-header bg-fluence-blue-gradient">
                         <div className="widget-user-image">
                             <span className="entity-info-box-icon">
-                                {/*<i className={app ? 'ion ion-ios-gear-outline' : 'fa fa-refresh fa-spin'}/>*/}
+                                <i className="ion ion-ios-checkmark-outline"/>
                             </span>
                         </div>
                         <h3 className="widget-user-username">Connect to {this.props.app.name}</h3>
@@ -29,6 +72,7 @@ class Snippets extends React.Component<Props, State> {
                     </div>
                     <div className="box-footer no-padding">
                         <div className="box-body">
+                            {this.props.trxHash && <p>Transaction hash: <a href={'https://rinkeby.etherscan.io/tx/' + this.props.trxHash} title={this.props.trxHash} className="etherscan-link" target="_blank">{cutId(this.props.trxHash)}</a></p>}
                             <p>Install dependency:</p>
                             <pre>npm install --save fluence@0.1.18</pre>
                             <p>Connect to {this.props.app.name}, add snippet to a JS file:</p>
@@ -52,10 +96,29 @@ session.request("SELECT AVG(age) FROM users").result().then((r) => {
 });
                             `}
                             </pre>
+                            <strong><i className="fa fa-bullseye margin-r-5"></i>Cluster</strong>
+                            {appInfo && <FluenceCluster appId={this.props.appId} cluster={appInfo.cluster}/>}
                         </div>
                     </div>
                 </div>
             )
+        } else if (this.props.deployState) {
+            return (
+                <div className="box box-widget widget-user-2">
+                    <div className="widget-user-header bg-fluence-blue-gradient">
+                        <div className="widget-user-image">
+                            <span className="entity-info-box-icon"><i className="fa fa-refresh fa-spin"></i></span>
+                        </div>
+                        <h3 className="widget-user-username">Deploying app</h3>
+                        <h3 className="widget-user-desc">...</h3>
+                    </div>
+                    <div className="box-footer no-padding">
+                        <div className="box-body">
+                            <p>Status: {this.getDeployStateLabel(this.props.deployState)}</p>
+                        </div>
+                    </div>
+                </div>
+            );
         } else {
             return null
         }
@@ -64,11 +127,18 @@ session.request("SELECT AVG(age) FROM users").result().then((r) => {
 
 const mapStateToProps = (state: any) => {
     return {
-        app: state.snippets.app,
-        appId: state.snippets.appId
+        deployState: state.deploy.deployState,
+        app: state.deploy.app,
+        appId: state.deploy.appId,
+        trxHash: state.deploy.trxHash,
+        apps: state.apps,
     }
 };
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+    displayLoading,
+    hideLoading,
+    retrieveApp,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Snippets);
