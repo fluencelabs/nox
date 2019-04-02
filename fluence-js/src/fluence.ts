7a865f38818f6e6ce1315e21eb9b323835fabec3
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-04-02 14:28:13 +0300

    revert ts

--- fluence-js/src/fluence.ts
+++ fluence-js/src/fluence.ts
@@ -1,18 +1,18 @@
 /*
-* Copyright 2018 Fluence Labs Limited
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2018 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import {TendermintClient} from "./TendermintClient";
 import {Engine} from "./Engine";
@@ -24,27 +24,27 @@ import {remove0x, secp256k1} from "./utils";
 import {AppSession} from "./AppSession";
 
 export {
-TendermintClient as TendermintClient,
-Engine as Engine,
-Session as Session,
-Result as Result,
-SessionConfig as SessionConfig,
-AppSession as AppSession
+    TendermintClient as TendermintClient,
+    Engine as Engine,
+    Session as Session,
+    Result as Result,
+    SessionConfig as SessionConfig,
+    AppSession as AppSession
 }
 
 // A session with a worker with info about a worker
 export interface WorkerSession {
-session: Session,
-node: Node
+    session: Session,
+    node: Node
 }
 
 /**
-* Creates a connection with an app (all nodes hosting an app)
-* @param contract Contract address to read app's nodes list from
-* @param appId Target app
-* @param ethereumUrl Optional ethereum node url. Connect via Metamask if `ethereumUrl` is undefined
-* @param privateKey Optional private key to sign requests. Signature is concatenated to the request payload.
-*/
+ * Creates a connection with an app (all nodes hosting an app)
+ * @param contract Contract address to read app's nodes list from
+ * @param appId Target app
+ * @param ethereumUrl Optional ethereum node url. Connect via Metamask if `etheremlUrl` is undefined
+ * @param privateKey Optional private key to sign requests. Signature is concatenated to the request payload.
+ */
 export async function connect(contract: string, appId: string, ethereumUrl?: string, privateKey?: Buffer | string): Promise<AppSession> {
     if (privateKey != undefined && typeof privateKey == 'string') {
         privateKey = Buffer.from(remove0x(privateKey), "hex");
