1ca6f687fb48cfcf719382b552a2a8da98a40168
folex <0xdxdy@gmail.com>
2019-04-08 19:28:30 +0300

    Use fullnode in dashboard & deploy

--- dashboard/src/constants.ts
+++ dashboard/src/constants.ts
@@ -2,7 +2,8 @@ export const defaultContractAddress: string = '0xeFF91455de6D4CF57C141bD8bF819E5
 export const stageContractAddress: string = '0xe01690f60E08207Fa29F9ef98fA35e7fB7A12A96';
 export const rootTagId: string = 'root';
 
-export const fluenceNodeAddr = 'http://data.fluence.one:8545';
+export const fluenceNodeAddr = 'http://rinkeby.fluence.one:8545';
+export const fluenceLightNodeAddr = 'http://data.fluence.one:8545';
 
 export const account = "0x7DdAE2d6118562AaC405284bb297C9A53d975326";
 export const privateKey = Buffer.from("3639CC2D3D27ABF76509077EFC0BE6093290F0F8739C00BDDA6504B9D9FC66C2", 'hex');
