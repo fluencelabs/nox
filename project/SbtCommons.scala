e71c69c48c7df3c4a73f7a391d8e42806779b0d1
folex <0xdxdy@gmail.com>
2019-04-17 22:47:31 +0300

    piririn

--- project/SbtCommons.scala
+++ project/SbtCommons.scala
@@ -78,49 +78,49 @@ object SbtCommons {
 
   val asmble = "com.github.cretz.asmble" % "asmble-compiler" % "0.4.4-fl"
 
-  val slogging = "biz.enef"        %% "slogging"    % "0.6.1"
-  val cats = "org.typelevel"       %% "cats-core"   % "1.5.0"
-  val catsEffect = "org.typelevel" %% "cats-effect" % "1.2.0"
+  val slogging =   "biz.enef"       %% "slogging"    % "0.6.1"
+  val cats =       "org.typelevel"  %% "cats-core"   % "1.5.0"
+  val catsEffect = "org.typelevel"  %% "cats-effect" % "1.2.0"
 
   val fs2Version = "1.0.4"
-  val fs2 = "co.fs2"   %% "fs2-core"             % fs2Version
+  val fs2 =   "co.fs2" %% "fs2-core"             % fs2Version
   val fs2rx = "co.fs2" %% "fs2-reactive-streams" % fs2Version
   val fs2io = "co.fs2" %% "fs2-io"               % fs2Version
 
   // functional wrapper around 'lightbend/config'
-  @deprecated("pureConfig is known to cause mindblowing failures on compile time, let's drop it away", "29.03.2019")
+//  @deprecated("pureConfig is known to cause mindblowing failures on compile time, let's drop it away", "29.03.2019")
   val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.10.2"
-  val ficus = "com.iheart"                 %% "ficus"      % "1.4.5"
+  val ficus =      "com.iheart"            %% "ficus"      % "1.4.5"
 
   val cryptoVersion = "0.0.3"
-  val cryptoHashing = "one.fluence"  %% "crypto-hashsign" % cryptoVersion
-  val cryptoCipher = "one.fluence"   %% "crypto-cipher"   % cryptoVersion
+  val cryptoHashing =  "one.fluence" %% "crypto-hashsign" % cryptoVersion
+  val cryptoCipher =   "one.fluence" %% "crypto-cipher"   % cryptoVersion
   val cryptoKeyStore = "one.fluence" %% "crypto-keystore" % cryptoVersion
-  val codecCore = "one.fluence"      %% "codec-core"      % "0.0.4"
+  val codecCore =      "one.fluence" %% "codec-core"      % "0.0.4"
 
   val sttpVersion = "1.5.12"
-  val sttp = "com.softwaremill.sttp"            %% "core"                           % sttpVersion
-  val sttpCirce = "com.softwaremill.sttp"       %% "circe"                          % sttpVersion
-  val sttpFs2Backend = "com.softwaremill.sttp"  %% "async-http-client-backend-fs2"  % sttpVersion
-  val sttpCatsBackend = "com.softwaremill.sttp"        %% "async-http-client-backend-cats" % sttpVersion
+  val sttp =            "com.softwaremill.sttp" %% "core"                           % sttpVersion
+  val sttpCirce =       "com.softwaremill.sttp" %% "circe"                          % sttpVersion
+  val sttpFs2Backend =  "com.softwaremill.sttp" %% "async-http-client-backend-fs2"  % sttpVersion
+  val sttpCatsBackend = "com.softwaremill.sttp" %% "async-http-client-backend-cats" % sttpVersion
 
   val http4sVersion = "0.20.0-M7"
-  val http4sDsl = "org.http4s"    %% "http4s-dsl"          % http4sVersion
+  val http4sDsl =    "org.http4s" %% "http4s-dsl"          % http4sVersion
   val http4sServer = "org.http4s" %% "http4s-blaze-server" % http4sVersion
-  val http4sCirce = "org.http4s"  %% "http4s-circe"        % http4sVersion
+  val http4sCirce =  "org.http4s" %% "http4s-circe"        % http4sVersion
 
   val circeVersion = "0.11.1"
-  val circeCore = "io.circe"          %% "circe-core"           % circeVersion
-  val circeGeneric = "io.circe"       %% "circe-generic"        % circeVersion
+  val circeCore =          "io.circe" %% "circe-core"           % circeVersion
+  val circeGeneric =       "io.circe" %% "circe-generic"        % circeVersion
   val circeGenericExtras = "io.circe" %% "circe-generic-extras" % circeVersion
-  val circeParser = "io.circe"        %% "circe-parser"         % circeVersion
+  val circeParser =        "io.circe" %% "circe-parser"         % circeVersion
 
   val scodecBits = "org.scodec" %% "scodec-bits" % "1.1.9"
   val scodecCore = "org.scodec" %% "scodec-core" % "1.11.3"
 
   val web3jVersion = "4.2.0"
-  val web3jCrypto = "org.web3j" % "crypto" % web3jVersion
-  val web3jCore = "org.web3j"   % "core"   % web3jVersion
+  val web3jCrypto =  "org.web3j" % "crypto" % web3jVersion
+  val web3jCore =    "org.web3j" % "core"   % web3jVersion
 
   val toml = "com.electronwill.night-config" % "toml" % "3.4.2"
 
@@ -131,7 +131,7 @@ object SbtCommons {
 
   /* Test deps*/
 
-  val scalaTest = "org.scalatest"            %% "scalatest"   % "3.0.5"  % Test
+  val scalaTest =            "org.scalatest" %% "scalatest"   % "3.0.5"  % Test
   val scalaIntegrationTest = "org.scalatest" %% "scalatest"   % "3.0.5"  % IntegrationTest
-  val mockito = "org.mockito"                % "mockito-core" % "2.21.0" % Test
+  val mockito =              "org.mockito"   % "mockito-core" % "2.21.0" % Test
 }
