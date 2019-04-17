cae4a03c50f82cf7ecf95b6e0a1c686209194ef9
folex <0xdxdy@gmail.com>
2019-04-17 15:59:38 +0300

    Runner: multi-session support + race condition/livelock fix (#643)

--- vm/flrun/src/main/scala/Main.scala
+++ vm/flrun/src/main/scala/Main.scala
@@ -14,13 +14,12 @@
  * limitations under the License.
  */
 
-import cats.effect.concurrent.{MVar, Ref}
 import cats.effect.{ExitCode, IO, IOApp}
 import fluence.vm.WasmVm
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.CORS
-import org.http4s.{HttpApp, HttpRoutes}
+import org.http4s.{HttpApp, HttpRoutes, Response}
 import slogging.LogLevel
 
 object Main extends IOApp with slogging.LazyLogging {
@@ -32,21 +31,21 @@ object Main extends IOApp with slogging.LazyLogging {
 
   // apps/1/tx
   // apps/1/query?path=kALX917gZsqm%2F0&data=
-  def routes(handler: Handler): HttpRoutes[IO] = HttpRoutes.of[IO] {
+  def routes(handler: TxProcessor[IO]): HttpRoutes[IO] = HttpRoutes.of[IO] {
     case req @ POST -> Root / "apps" / LongVar(appId) / "tx" ⇒
       logger.info(s"Tx request. appId: $appId")
       req.decode[String] { input ⇒
         val Array(path, tx) = input.split('\n')
         logger.info(s"Tx: '$tx'")
-        handler.processTx(Tx(appId, path, tx))
+        handler.processTx(Tx(appId, path, tx)).handleErrorWith(e => BadRequest(e.getMessage))
       }
 
     case GET -> Root / "apps" / LongVar(appId) / "query" :? QueryPath(path) +& QueryData(data) ⇒
       logger.info(s"Query request. appId: $appId, path: $path, data: $data")
-      handler.processQuery(Query(appId, path))
+      handler.processQuery(Query(appId, path)).handleErrorWith(e => BadRequest(e.getMessage))
   }
 
-  def app(handler: Handler): HttpApp[IO] =
+  def app(handler: TxProcessor[IO]): HttpApp[IO] =
     CORS[IO, IO](routes(handler).orNotFound, corsConfig)
 
   override def run(args: List[String]): IO[ExitCode] = {
@@ -55,11 +54,8 @@ object Main extends IOApp with slogging.LazyLogging {
       files <- getWasmFiles()
       vmOrError <- WasmVm[IO](files, "fluence.vm.debugger").value
       vm <- IO.fromEither(vmOrError)
-      map <- Ref.of[IO, Map[String, String]](Map.empty[String, String])
-      mutex <- MVar.empty[IO, Unit]
-      ref <- Ref[IO].of(-1)
-      order = Order(ref)
-      httpApp = app(Handler(vm, map, mutex, order))
+      processor <- TxProcessor[IO](vm)
+      httpApp = app(processor)
       res = BlazeServerBuilder[IO].withBanner(Nil).bindHttp(Port, Host).withHttpApp(httpApp).resource
       _ <- res.use(_ => IO.never)
     } yield ExitCode.Success
