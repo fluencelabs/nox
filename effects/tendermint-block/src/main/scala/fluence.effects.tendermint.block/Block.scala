dc45ef0a32f7b3309a3a693013b0d2203ab813c5
folex <0xdxdy@gmail.com>
2019-04-18 14:57:56 +0300

    WIP

--- effects/tendermint-block/src/main/scala/fluence.effects.tendermint.block/Block.scala
+++ effects/tendermint-block/src/main/scala/fluence.effects.tendermint.block/Block.scala
@@ -1,40 +1,26 @@
+/*
+ * Copyright 2018 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fluence.effects.tendermint.block
 
 import jdk.internal.org.objectweb.asm.ByteVector
-import proto3.block.Header
+import proto3.tendermint.{Header, Vote}
+import scalapb_circe.JsonFormat
 
-// https://tendermint.com/docs/spec/blockchain/blockchain.html#blockid
-case class BlockId(hash: String, partsCount: Int, partsHash: String)
-
-case class BlockHeader(
-                        version: String,
-                        chain_id: String,
-                        height: String,
-                        time: String,
-                        num_txs: String,
-                        total_txs: String,
-
-                        // id of the previous block
-                        last_block_id: BlockId,
-                        // commit from the previous block
-                        last_commit_hash: String,
-                        // MerkleRoot of transaction hashes
-                        data_hash: String,
-
-                        // === hashes from the app output from the prev block ===
-                        // hash(list of this block's validators)
-                        validators_hash: String,
-                        // hash(list of next block's validators)
-                        next_validators_hash: String,
-                        // hash(consensus parameters)
-                        consensus_hash: String,
-                        // hash(app state after txs from previous block)
-                        app_hash: String,
-                        // root hash of all results from the txs from the previous block
-                        last_results_hash: String,
-                        evidence_hash: String,
-                        proposer_address: String,
-                      )
+// About BlockID: https://tendermint.com/docs/spec/blockchain/blockchain.html#blockid
 
 // TODO: to/from JSON
 case class Block(header: Header, txs: List[_]) {
@@ -42,7 +28,7 @@ case class Block(header: Header, txs: List[_]) {
   type Hash = ByteVector
   type Tx = ByteVector
   type Evidence = ByteVector
-  type Precommits = List[proto3.Vote] // also Vote = CommitSig in Go
+  type Precommits = List[Vote] // also, Vote = CommitSig in Go
 
   // SimpleHash, go: SimpleHashFromByteSlices
   // https://github.com/tendermint/tendermint/wiki/Merkle-Trees#simple-tree-with-dictionaries
@@ -69,18 +55,18 @@ case class Block(header: Header, txs: List[_]) {
 		b.LastCommitHash = b.LastCommit.Hash() // commitHash
 		b.DataHash = b.Data.Hash() // txsHash
 		b.EvidenceHash = b.Evidence.Hash()
-  */
+   */
 
   def headerHash(): ByteVector = {
     fillHeader() // don't forget it's already called in blockHash (meh)
-    val data = List (
+    val data = List(
       Amino.encode(header.version),
-      Amino.encode(header.chainID),
+      Amino.encode(header.chainId),
       Amino.encode(header.height),
       Amino.encode(header.time),
       Amino.encode(header.numTxs),
       Amino.encode(header.totalTxs),
-      Amino.encode(header.lastBlockID),
+      Amino.encode(header.lastBlockId),
       Amino.encode(header.lastCommitHash),
       Amino.encode(header.dataHash),
       Amino.encode(header.validatorsHash),
@@ -96,13 +82,13 @@ case class Block(header: Header, txs: List[_]) {
   }
 
   // Merkle hash of all precommits (some of them could be null?)
-  def commitHash(precommits: List[proto3.Vote]) = ???
+  def commitHash(precommits: List[Vote]) = ???
   /*
     for i, precommit := range commit.Precommits {
 			bs[i] = cdcEncode(precommit)
 		}
 		commit.hash = merkle.SimpleHashFromByteSlices(bs)
-  */
+   */
 
   // Merkle hash from the list of TXs
   def txsHash(txs: List[Tx]) = Merkle.simpleHash(txs)
@@ -111,7 +97,7 @@ case class Block(header: Header, txs: List[_]) {
       txBzs[i] = txs[i].Hash()
     }
     return merkle.SimpleHashFromByteSlices(txBzs)
-  */
+   */
 
   // Hash of the single tx, go: tmhash.Sum(tx) -> SHA256.sum
   def singleTxHash(tx: Tx) = SHA256.sum(tx)
@@ -122,7 +108,7 @@ case class Block(header: Header, txs: List[_]) {
       evidenceBzs[i] = evl[i].Bytes()
     }
     return merkle.SimpleHashFromByteSlices(evidenceBzs)
-  */
+ */
 
 }
 
@@ -136,4 +122,28 @@ object SHA256 {
 
 object Amino {
   def encode(any: Any): ByteVector = ???
+}
+
+object JSON {
+
+  val firstVote =
+    """
+      |{
+      |    "type": 2,
+      |    "height": 16,
+      |    "round": 0,
+      |    "block_id": {
+      |        "hash": "1E56CF404964AA6B0768E67AD9CBACABCEBCD6A84DC0FC924F1C0AF9043C0188",
+      |        "parts": {
+      |            "total": 1,
+      |            "hash": "D0A00D1902638E1F4FD625568D4A4A7D9FC49E8F3586F257535FC835E7B0B785"
+      |        }
+      |    },
+      |    "timestamp": "2019-04-17T13:30:03.536359799Z",
+      |    "validator_address": "04C60B72246943675E2F3AADA00E30EC41AA7D4E",
+      |    "validator_index": 0,
+      |    "signature": "Z09xcrfz9T6+3q1Yk+gxUo2todPI7mebKed6zO+i1pnIMPdFbSFT9JJjxo5J9HLrn4x2Fqf3QYefQ8lQGNMzBg=="
+      |}
+    """.stripMargin
+  def vote(json: String): Vote = JsonFormat.fromJsonString[Vote](json)
 }
