a82f5eefb396754863e742d3576e29237a98ffb1
folex <0xdxdy@gmail.com>
2019-04-24 13:52:19 +0300

    Rename dotted directory

--- effects/tendermint-block/src/main/scala/fluence.effects.tendermint.block/errors/ValidationError.scala
+++
@@ -1,7 +0,0 @@
-package fluence.effects.tendermint.block.errors
-
-object ValidationError {
-  sealed trait ValidationError extends TendermintBlockError
-  case class InvalidDataHash(expected: String, actual: String) extends ValidationError
-  case class InvalidCommitHash(expected: String, actual: String) extends ValidationError
-}
