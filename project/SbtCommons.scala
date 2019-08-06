e17e0ed9d102dee9c9f29f7340c4d7b6ed26bbb9
folex <0xdxdy@gmail.com>
2019-08-06 18:57:10 +0300

    back to web3j 4.2.0

--- project/SbtCommons.scala
+++ project/SbtCommons.scala
@@ -138,7 +138,7 @@ object SbtCommons {
   val scodecBits = "org.scodec" %% "scodec-bits" % "1.1.9"
   val scodecCore = "org.scodec" %% "scodec-core" % "1.11.3"
 
-  val web3jVersion = "4.3.0"
+  val web3jVersion = "4.2.0"
   val web3jCrypto = "org.web3j" % "crypto" % web3jVersion
   val web3jCore = "org.web3j"   % "core"   % web3jVersion
 
