2b26185c984a828ccf43d426a78acfdc6d2a8d8f
folex <0xdxdy@gmail.com>
2020-06-10 19:37:31 +0300

    enable libp2p_kad::behaviour logs

--- server/tests/utils/misc.rs
+++ server/tests/utils/misc.rs
@@ -130,7 +130,7 @@ HFF3V9XXbhdTLWGVZkJYd9a7NyuD5BLWLdwc4EFBcCZa
 #[allow(dead_code)]
 // Enables logging, filtering out unnecessary details
 pub fn enable_logs() {
-    use log::LevelFilter::{Debug, Info, Warn};
+    use log::LevelFilter::{Debug, Info};
 
     env_logger::builder()
         .filter_level(Debug)
@@ -149,7 +149,6 @@ pub fn enable_logs() {
         .filter(Some("libp2p_kad::kbucket"), Info)
         .filter(Some("libp2p_plaintext"), Info)
         .filter(Some("libp2p_identify::protocol"), Info)
-        .filter(Some("libp2p_kad::behaviour"), Warn)
         .try_init()
         .ok();
 }
