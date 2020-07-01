6c8f43a16a117950bc9e161da40b50486225de55
folex <0xdxdy@gmail.com>
2020-07-01 14:20:38 +0300

    Set cors in built-in ipfs

--- deploy/fluence-ipfs/run_fluence_ipfs.sh
+++ deploy/fluence-ipfs/run_fluence_ipfs.sh
@@ -8,8 +8,17 @@ BASH=$(command -v bash)
 IPFS=$(command -v ipfs)
 
 ipfs init --profile server >/dev/null || true
+# Announce public ip to the network, so other IPFS nodes know how to reach us
 ipfs config --json Addresses.Announce \[\"/ip4/"$HOST"/tcp/4001\"\]
+# Listen on all interfaces inside container (default is localhost)
 ipfs config --json Addresses.API '"/ip4/0.0.0.0/tcp/5001/"'
+# CORS
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Headers '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Headers '["*"]'
+ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
 
 # Run Server & IPFS in parallel, fail if any of the processes fails
 $BASH wait.sh \
