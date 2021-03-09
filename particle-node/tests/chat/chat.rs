820d40939080078da1d982e4b636c0daff07449d
folex <0xdxdy@gmail.com>
2021-03-09 11:08:53 +0300

    remove 'enable_logs'

--- particle-node/tests/chat/chat.rs
+++ particle-node/tests/chat/chat.rs
@@ -36,7 +36,7 @@
 use config_utils::to_abs_path;
 use json_utils::into_array;
 use particle_providers::Provider;
-use test_utils::{connect_swarms, enable_logs, module_config, ConnectedClient};
+use test_utils::{connect_swarms, module_config, ConnectedClient};
 
 use eyre::{ContextCompat, WrapErr};
 use fstrings::f;
@@ -285,8 +285,6 @@ fn send_message(msg: &str, author: &str, client: &mut ConnectedClient) {
 
 #[test]
 fn test_chat() {
-    enable_logs();
-
     let node_count = 5;
     let connect = connect_swarms(node_count);
     let mut client = connect(0);
