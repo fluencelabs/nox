42227f74c7eb58b307ccf7e135b89693a70e4ec2
folex <0xdxdy@gmail.com>
2021-03-22 22:40:38 +0300

    reorder benches

--- particle-node/benches/network_api_bench.rs
+++ particle-node/benches/network_api_bench.rs
@@ -355,9 +355,9 @@ fn particle_throughput_with_delay_bench(c: &mut Criterion) {
     let particle_timeout = TIMEOUT;
 
     let mut group = c.benchmark_group("particle_throughput_with_delay");
-    for &num in [1, 1000, 2 * 1000, 4 * 1000].iter() {
-        for &pool_size in [1, 2, 4, 8, 16].iter() {
-            for delay in [None, Some(Duration::from_nanos(1))].iter() {
+    for &num in [1, 1000, 4 * 1000, 4 * 1000].iter() {
+        for delay in [None, Some(Duration::from_millis(1))].iter() {
+            for &pool_size in [1, 2, 4, 16, 256].iter() {
                 group.throughput(Throughput::Elements(num as u64));
                 let bid = {
                     let delay = delay.unwrap_or(Duration::from_nanos(0));
