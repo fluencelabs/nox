7075ebfac2cb2884cbd3b357f971061357e97c5e
folex <0xdxdy@gmail.com>
2021-06-09 20:57:18 +0300

    Builtins: support array-backed options (#1115)

--- crates/host-closure/src/args.rs
+++ crates/host-closure/src/args.rs
@@ -99,11 +99,100 @@ impl Args {
         Ok(Some(value))
     }
 
+    /// Retrieves a json value from iterator if it's not empty, and parses it to Aqua's option representation
+    /// Aqua's option is expected to be an array of 1 or 0 elements.
+    /// For the sakes of backward compatibility, scalar value and absence of value are tolerated as well.
+    /// `field` is to generate a more accurate error message
+    ///
+    /// In short, function returns:
+    /// - if next arg is `T` or `[T]`               => Some(T)
+    /// - if next arg is `None` or `[]`             => None
+    /// - if next arg is array of several elements  => error
+    pub fn next_opt<T: for<'de> Deserialize<'de>>(
+        field: &'static str,
+        args: &mut impl Iterator<Item = JValue>,
+    ) -> Result<Option<T>, ArgsError> {
+        let value = ok_get!(args.next());
+        let value = match value {
+            // If there is more than 1 value in the passed array
+            // then, depending on T, it may be one of:
+            //    - Some(array) if T is an array
+            //    - invalid option if T is a scalar (because you can't deserialize several values as a scalar)
+            // The only way to tell one from the other is by looking at the T.
+            // And that's what is done here:
+            // keep value untouched, so T can deserialize itself and fail as needed
+            JValue::Array(values) if values.len() > 1 => JValue::Array(values),
+            // Option backed by array of 1 or 0 elements
+            JValue::Array(values) => {
+                ok_get!(values.into_iter().next())
+            }
+            // Scalar value
+            value => value,
+        };
+
+        let value: T = Self::deserialize(field, value)?;
+        Ok(Some(value))
+    }
+
     /// `field` is to generate a more accurate error message
     fn deserialize<T: for<'de> Deserialize<'de>>(
         field: &'static str,
         v: JValue,
     ) -> Result<T, ArgsError> {
         serde_json::from_value(v).map_err(|err| SerdeJson { err, field })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Args;
+    use serde_json::json;
+
+    #[test]
+    fn test_next_opt() {
+        #[rustfmt::skip]
+        let mut args = vec![
+            json!([]),           // as String => None
+            json!(["hi"]),       // as String => Some
+            json!(["hi", "hi"]), // as Vec    => Some
+            json!([]),           // as Vec    => None
+            json!(["hi", "hi"]), // as String => Error
+            json!("hi"),         // as String => Some
+            /* absence */        // as String => None
+            /* absence */        // as Vec    => None
+        ]
+        .into_iter();
+
+        fn hi() -> String {
+            "hi".to_string()
+        }
+
+        let none: Result<Option<String>, _> = Args::next_opt("", &mut args);
+        assert_eq!(none.unwrap(), None);
+
+        let some: Result<Option<String>, _> = Args::next_opt("", &mut args);
+        assert_eq!(some.unwrap(), Some(hi()));
+
+        let some_vec: Result<Option<Vec<String>>, _> = Args::next_opt("", &mut args);
+        assert_eq!(some_vec.unwrap(), Some(vec![hi(), hi()]));
+
+        let none_vec: Result<Option<Vec<String>>, _> = Args::next_opt("", &mut args);
+        assert_eq!(none_vec.unwrap(), None);
+
+        let scalar_err: Result<Option<String>, _> = Args::next_opt("scalar", &mut args);
+        assert!(scalar_err.is_err());
+        assert_eq!(
+            "Error while deserializing field scalar: invalid type: sequence, expected a string",
+            scalar_err.err().unwrap().to_string()
+        );
+
+        let some: Result<Option<String>, _> = Args::next_opt("", &mut args);
+        assert_eq!(some.unwrap(), Some(hi()));
+
+        let none: Result<Option<String>, _> = Args::next_opt("", &mut args);
+        assert_eq!(none.unwrap(), None);
+
+        let none_vec: Result<Option<Vec<String>>, _> = Args::next_opt("", &mut args);
+        assert_eq!(none_vec.unwrap(), None);
     }
 }
