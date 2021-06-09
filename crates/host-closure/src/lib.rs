dd03fadfdbfb15305d555d2fad03c6cc6998d4f5
folex <0xdxdy@gmail.com>
2021-06-10 00:25:27 +0300

    remove closures!

--- crates/host-closure/src/lib.rs
+++ crates/host-closure/src/lib.rs
@@ -12,14 +12,13 @@
 mod args;
 mod args_error;
 mod base58;
-mod closure;
 
 pub use args::Args;
 pub use args_error::{ArgsError, JError};
-pub use closure::{
-    closure, closure_args, closure_opt, closure_params, closure_params_opt, Closure,
-    ClosureDescriptor, ParticleClosure,
-};
 
 pub use avm_server::ParticleParameters;
 pub use base58::from_base58;
+
+use avm_server::CallServiceClosure;
+use std::sync::Arc;
+pub type ClosureDescriptor = Arc<dyn Fn() -> CallServiceClosure + Send + Sync + 'static>;
