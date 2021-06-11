d4b4ab9fae2076c85411176ae64ffb4e361a26c8
folex <0xdxdy@gmail.com>
2021-06-11 19:08:52 +0300

    --bootstrap-freq, --allow-private-ips

--- particle-node/src/config/args.rs
+++ particle-node/src/config/args.rs
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-use server_config::config_keys::*;
-
 use clap::Arg;
+
+use server_config::config_keys::*;
 
 pub fn create_args<'help>() -> Vec<Arg<'help>> {
     vec![
@@ -46,8 +46,17 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .long("bootstraps")
             .multiple(true)
             .about("bootstrap nodes of the Fluence network"),
-        Arg::new(LOCAL)
+        Arg::new(BOOTSTRAP_FREQ)
             .display_order(3)
+            .help_heading(Some("Networking"))
+            .value_name("N")
+            .takes_value(true)
+            .short('q')
+            .long("bootstrap-freq")
+            .multiple(true)
+            .about("bootstrap kademlia each time N bootstraps (re)connect"),
+        Arg::new(LOCAL)
+            .display_order(4)
             .help_heading(Some("Networking"))
             .short('l')
             .long("local")
@@ -55,7 +64,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .conflicts_with(BOOTSTRAP_NODE)
             .about("if passed, bootstrap nodes aren't used"),
         Arg::new(EXTERNAL_ADDR)
-            .display_order(4)
+            .display_order(5)
             .help_heading(Some("Networking"))
             .takes_value(true)
             .short('x')
@@ -63,7 +72,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .value_name("IP")
             .about("node external IP address to advertise to other peers"),
         Arg::new(EXTERNAL_MULTIADDRS)
-            .display_order(5)
+            .display_order(6)
             .help_heading(Some("Networking"))
             .takes_value(true)
             .multiple(true)
@@ -71,9 +80,17 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .long("external-maddrs")
             .value_name("MULTIADDR")
             .about("external multiaddresses to advertize"),
+        Arg::new(ALLOW_LOCAL)
+            .display_order(7)
+            // .default_value("disabled")
+            .help_heading(Some("Networking"))
+            .short('a')
+            .long("allow-private-ips")
+            .takes_value(false)
+            .about("allow private IP addresses from other nodes"),
         // keypair
         Arg::new(ROOT_KEY_PAIR_VALUE)
-            .display_order(6)
+            .display_order(8)
             .help_heading(Some("Node keypair"))
             .takes_value(true)
             .short('k')
@@ -82,7 +99,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .about("keypair in base58 (conflicts with --keypair-path)")
             .conflicts_with(ROOT_KEY_PAIR_PATH),
         Arg::new(ROOT_KEY_PAIR_PATH)
-            .display_order(7)
+            .display_order(9)
             .help_heading(Some("Node keypair"))
             .takes_value(true)
             .short('p')
@@ -90,14 +107,14 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .about("keypair path (conflicts with --keypair-value)")
             .conflicts_with(ROOT_KEY_PAIR_VALUE),
         Arg::new(ROOT_KEY_PAIR_FORMAT)
-            .display_order(8)
+            .display_order(10)
             .help_heading(Some("Node keypair"))
             .takes_value(true)
             .short('f')
             .long("keypair-format")
             .possible_values(&["ed25519", "secp256k1", "rsa"]),
         Arg::new(ROOT_KEY_PAIR_GENERATE)
-            .display_order(9)
+            .display_order(11)
             .help_heading(Some("Node keypair"))
             .takes_value(true)
             .short('g')
@@ -105,7 +122,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .about("generate keypair on absence"),
         // node configuration
         Arg::new(CONFIG_FILE)
-            .display_order(10)
+            .display_order(12)
             .help_heading(Some("Node configuration"))
             .takes_value(true)
             .short('c')
@@ -113,7 +130,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .value_name("PATH")
             .about("TOML configuration file"),
         Arg::new(CERTIFICATE_DIR)
-            .display_order(11)
+            .display_order(13)
             .help_heading(Some("Node configuration"))
             .takes_value(true)
             .short('d')
@@ -121,7 +138,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .value_name("PATH")
             .about("certificate dir"),
         Arg::new(MANAGEMENT_PEER_ID)
-            .display_order(12)
+            .display_order(14)
             .help_heading(Some("Node configuration"))
             .takes_value(true)
             .long("management-key")
@@ -131,7 +148,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .about("PeerId of the node's administrator"),
         // services
         Arg::new(SERVICE_ENVS)
-            .display_order(13)
+            .display_order(15)
             .help_heading(Some("Services configuration"))
             .value_name("NAME=VALUE")
             .takes_value(true)
@@ -140,7 +157,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .multiple(true)
             .about("envs to pass to core modules"),
         Arg::new(BLUEPRINT_DIR)
-            .display_order(14)
+            .display_order(16)
             .help_heading(Some("Services configuration"))
             .takes_value(true)
             .short('u')
@@ -148,7 +165,7 @@ pub fn create_args<'help>() -> Vec<Arg<'help>> {
             .value_name("PATH")
             .about("directory containing blueprints and wasm modules"),
         Arg::new(SERVICES_WORKDIR)
-            .display_order(15)
+            .display_order(17)
             .help_heading(Some("Services configuration"))
             .takes_value(true)
             .short('r')
