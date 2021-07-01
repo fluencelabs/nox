5051ce72d052cdc5caf1879c71170e9405d803a2
folex <0xdxdy@gmail.com>
2021-07-01 18:35:43 +0300

    use builtin aquadht

--- particle-node/tests/aqua_dht/aqua_dht.rs
+++ particle-node/tests/aqua_dht/aqua_dht.rs
@@ -15,54 +15,15 @@
  */
 
 use connected_client::ConnectedClient;
-use created_swarm::make_swarms;
+use created_swarm::make_swarms_with_builtins;
 use eyre::{ContextCompat, WrapErr};
 use maplit::hashmap;
 use serde_json::json;
 use service_modules::{load_module, module_config};
+use std::path::Path;
 
 fn load_script(name: &str) -> String {
     std::fs::read_to_string(format!("./tests/aqua_dht/aqua/{}", name)).unwrap()
-}
-
-fn create_service(client: &mut ConnectedClient, module: &str) -> String {
-    let script = r#"
-        (seq
-            (seq
-                (call node ("dist" "add_module") [module_bytes module_config])
-                (call node ("dist" "add_module") [sqlite_bytes sqlite_config])
-            )
-            (seq
-                (call node ("dist" "add_blueprint") [blueprint] blueprint_id)
-                (seq
-                    (call node ("srv" "create") [blueprint_id] service_id)
-                    (call client ("return" "") [service_id] client_result)
-                )
-            )
-        )
-        "#;
-
-    let module_bytes = load_module("tests/aqua_dht/", module).expect("load aqua-dht wasm");
-    let module_hash = format!("hash:{}", blake3::hash(&module_bytes).to_hex().as_str());
-    let sqlite_module = load_module("tests/aqua_dht/", "sqlite3").expect("load sqlite wasm");
-    let data = hashmap! {
-        "client" => json!(client.peer_id.to_string()),
-        "node" => json!(client.node.to_string()),
-        "module_bytes" => json!(base64::encode(module_bytes)),
-        "module_config" => json!(module_config(module)),
-        "sqlite_bytes" => json!(base64::encode(sqlite_module)),
-        "sqlite_config" => json!(module_config("sqlite3")),
-        "blueprint" => json!({ "name": module, "dependencies": ["name:sqlite3", module_hash] }),
-    };
-
-    client.send_particle(script, data);
-    let response = client.receive_args().wrap_err("receive").unwrap();
-
-    response[0]
-        .as_str()
-        .wrap_err("missing service_id")
-        .unwrap()
-        .to_string()
 }
 
 #[test]
@@ -70,34 +31,11 @@ fn put_get() {
     let register_put_script = load_script("dht-api.registerKeyPutValue.air");
     let get_values_script = load_script("dht-api.getValues.air");
 
-    let swarms = make_swarms(3);
+    let swarms = make_swarms_with_builtins(3, Path::new("../deploy/builtins"), None);
 
     let mut client = ConnectedClient::connect_to(swarms[0].multiaddr.clone())
         .wrap_err("connect client")
         .unwrap();
-
-    for s in swarms {
-        let mut client = ConnectedClient::connect_with_keypair(
-            s.multiaddr.clone(),
-            Some(s.management_keypair.clone()),
-        )
-        .unwrap();
-        let dht_service = create_service(&mut client, "aqua-dht");
-        client.send_particle(
-            r#"
-            (seq
-                (call -relay- ("srv" "add_alias") [alias service])
-                (call %init_peer_id% ("op" "return") [])
-            )
-            "#,
-            hashmap! {
-                "-relay-" => json!(client.node.to_string()),
-                "service" => json!(dht_service),
-                "alias" => json!("aqua-dht"),
-            },
-        );
-        client.receive_args().unwrap();
-    }
 
     let key = "key";
     let put_particle = client.send_particle_ext(
