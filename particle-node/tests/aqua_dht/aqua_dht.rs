e9d203efd2d9fae0d8b9eea386390ae87542ae9b
folex <0xdxdy@gmail.com>
2021-07-01 18:15:51 +0300

    parse records

--- particle-node/tests/aqua_dht/aqua_dht.rs
+++ particle-node/tests/aqua_dht/aqua_dht.rs
@@ -17,11 +17,9 @@
 use connected_client::ConnectedClient;
 use created_swarm::make_swarms;
 use eyre::{ContextCompat, WrapErr};
-use log_utils::enable_logs;
 use maplit::hashmap;
 use serde_json::json;
 use service_modules::{load_module, module_config};
-use std::time::Duration;
 
 fn load_script(name: &str) -> String {
     std::fs::read_to_string(format!("./tests/aqua_dht/aqua/{}", name)).unwrap()
@@ -69,12 +67,10 @@ fn create_service(client: &mut ConnectedClient, module: &str) -> String {
 
 #[test]
 fn put_get() {
-    let put_script = load_script("dht-api.registerKeyPutValue.air");
-    let get_values = load_script("dht-api.getValues.air");
+    let register_put_script = load_script("dht-api.registerKeyPutValue.air");
+    let get_values_script = load_script("dht-api.getValues.air");
 
     let swarms = make_swarms(3);
-
-    enable_logs();
 
     let mut client = ConnectedClient::connect_to(swarms[0].multiaddr.clone())
         .wrap_err("connect client")
@@ -105,24 +101,24 @@ fn put_get() {
 
     let key = "key";
     let put_particle = client.send_particle_ext(
-        put_script,
+        register_put_script,
         hashmap! {
             "-relay-" => json!(client.node.to_string()),
             "node_id" => json!(client.node.to_string()),
             "key" => json!(key),
             "value" => json!("value"),
-            // relay_id & service_id are options, which are represented as arrays
+            // relay_id & service_id are of Option type, so they are represented as arrays
             "relay_id" => json!([client.node.to_string()]),
             "service_id" => json!(["service_id"]),
         },
         true,
     );
-    let result = client.wait_particle_args(&put_particle);
-    println!("result: {:?}", result);
-    std::thread::sleep(Duration::from_secs(1));
+    client
+        .wait_particle_args(&put_particle)
+        .expect("execute registerKeyPutValue");
 
-    let id = client.send_particle_ext(
-        get_values,
+    let get_particle = client.send_particle_ext(
+        get_values_script,
         hashmap! {
             "-relay-" => json!(client.node.to_string()),
             "node_id" => json!(client.node.to_string()),
@@ -131,6 +127,51 @@ fn put_get() {
         },
         true,
     );
-    let result = client.wait_particle_args(&id);
-    println!("result: {:?}", result);
+    let mut result = client
+        .wait_particle_args(&get_particle)
+        .expect("execute getValues");
+    let records = result.get_mut(0).unwrap().take();
+    let records: Vec<Record> = serde_json::from_value(records).unwrap();
+
+    for record in records {
+        assert_eq!(&record.value, "value");
+        assert_eq!(record.service_id, vec![String::from("service_id")]);
+    }
 }
+
+#[derive(serde::Deserialize, Debug)]
+struct Record {
+    value: String,
+    peer_id: String,
+    set_by: String,
+    relay_id: Vec<String>,
+    service_id: Vec<String>,
+    timestamp_created: u64,
+    weight: u32,
+}
+
+/*
+
+data Record:
+    value: string
+    peer_id: string
+    set_by: string
+    relay_id: []string
+    service_id: []string
+    timestamp_created: u64
+    weight: u32
+
+   {
+       "peer_id": "12D3KooWFoLti25GytKkEaZCbAgXFdHr3EBYmAxzbwf4fvLTBGEq",
+       "relay_id": [
+           "12D3KooWDHXBHVmyhxGbcQWAYFaeHkYdHTVcSdV9bwFEJZRdk8K7"
+       ],
+       "service_id": [
+           "service_id"
+       ],
+       "set_by": "12D3KooWFoLti25GytKkEaZCbAgXFdHr3EBYmAxzbwf4fvLTBGEq",
+       "timestamp_created": 1625147497,
+       "value": "value",
+       "weight": 0
+   },
+*/
