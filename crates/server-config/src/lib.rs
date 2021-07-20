43a9ce2e0ac8dab2deb12e5f2c48839bfba744a3
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2021-07-20 17:26:26 +0300

    fix

--- crates/server-config/src/lib.rs
+++ crates/server-config/src/lib.rs
@@ -40,7 +40,6 @@ mod resolved_config;
 mod services_config;
 
 pub use defaults::default_base_dir;
-pub use keys::load_key_pair;
 pub use resolved_config::{deserialize_config, load_config};
 
 pub use bootstrap_config::BootstrapConfig;
