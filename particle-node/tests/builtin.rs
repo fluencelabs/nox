ca22998c00e72a2759a9fbc4d5fc1a7a12757c03
Aleksey Proshutisnkiy <justprosh@users.noreply.github.com>
2021-08-12 14:44:29 +0300

    Builtin "kad merge": return only unique (#1171)
    Deduplicate elements in "kad merge" builtin; set default count to K_VALUE (20)

--- particle-node/tests/builtin.rs
+++ particle-node/tests/builtin.rs
@@ -34,6 +34,7 @@ use libp2p::core::Multiaddr;
 use libp2p::kad::kbucket::Key;
 
 use eyre::WrapErr;
+use itertools::Itertools;
 use libp2p::PeerId;
 use maplit::hashmap;
 use serde::Deserialize;
@@ -631,6 +632,7 @@ fn kad_merge() {
     let target = RandomPeerId::random();
     let left = (1..10).map(|_| RandomPeerId::random()).collect::<Vec<_>>();
     let mut right = (1..10).map(|_| RandomPeerId::random()).collect::<Vec<_>>();
+    right = right.into_iter().chain(left.clone().into_iter()).collect();
     let count = 10;
 
     let script = r#"
@@ -657,6 +659,7 @@ fn kad_merge() {
     let target_key = Key::from(target);
     let mut expected = left;
     expected.append(&mut right);
+    expected = expected.into_iter().unique().collect();
     expected.sort_by_cached_key(|id| target_key.distance(&Key::from(id.clone())));
     expected.truncate(count);
 
