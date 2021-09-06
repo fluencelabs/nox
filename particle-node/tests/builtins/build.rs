aa294a5835a2a4483dfd3a5e97668f3b3c471be4
folex <0xdxdy@gmail.com>
2021-09-06 13:19:23 +0300

    build.rs: download builtins

---
+++ particle-node/tests/builtins/build.rs
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use std::env;
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    // Note that there are a number of downsides to this approach, the comments
+    // below detail how to improve the portability of these commands.
+    let status = Command::new("download_builtins.sh")
+        .status()
+        .unwrap();
+    assert!(status.success(), "download_builtins failed with {:?}", status)
+    println!("cargo:rerun-if-changed=download_builtins.sh");
+}
---
+++ particle-node/tests/builtins/download_builtins.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -o pipefail -o nounset -o errexit
+
+# get script directory
+BUILTINS_DIR="$(dirname "$0")"
+SERVICES_DIR="${BUILTINS_DIR}/services"
+TAR="aqua-dht.tar.gz"
+
+echo "*** downloading $TAR ***"
+URL="https://github.com/fluencelabs/aqua-dht/releases/latest/download/aqua-dht.tar.gz"
+curl --fail -L "$URL" -o $TAR
+
+echo "*** extracting $TAR ***"
+tar -C $SERVICES_DIR -xf $TAR
+
+echo "*** done ***"
