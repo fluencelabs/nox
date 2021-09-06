3c2b24d28d3e403a199dc125409195feed5e0f70
folex <0xdxdy@gmail.com>
2021-09-06 18:25:55 +0300

    Integration Tests: download aqua-dht builtin (#1182)

---
+++ particle-node/tests/builtins/download_builtins.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o pipefail -o nounset -o errexit
+
+# get script directory
+BUILTINS_DIR="$(dirname "$0")"
+SERVICES_DIR="${BUILTINS_DIR}/services"
+TAR="aqua-dht.tar.gz"
+
+echo "*** downloading $TAR ***"
+URL="https://github.com/fluencelabs/aqua-dht/releases/latest/download/aqua-dht.tar.gz"
+curl --fail -L "$URL" -o "$TAR"
+
+echo "*** extracting $TAR ***"
+mkdir -p "$SERVICES_DIR"
+tar -C "$SERVICES_DIR" -xf "$TAR"
+
+rm "$TAR"
+
+echo "*** done ***"
---
+++ particle-node/tests/builtins/tests/tests.rs
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#[macro_use]
+extern crate fstrings;
+
+pub const SERVICES: &'static str = "./services";
+
+mod src {
+    mod aqua_dht {
+        mod aqua_dht;
+        mod pubsub;
+
+        #[derive(serde::Deserialize, Debug)]
+        pub struct Record {
+            value: String,
+            peer_id: String,
+            set_by: String,
+            relay_id: Vec<String>,
+            service_id: Vec<String>,
+            timestamp_created: u64,
+            weight: u32,
+        }
+    }
+
+    mod builtins_deployer;
+}
+
+pub fn load_script(name: &str) -> String {
+    std::fs::read_to_string(format!("./tests/src/aqua_dht/aqua/{}", name)).unwrap()
+}
