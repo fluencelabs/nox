d8fa58b8c8704743d627bf83d4871181740a9100
folex <0xdxdy@gmail.com>
2022-01-27 21:33:37 +0300

    add todos

--- crates/peer-metrics/src/lib.rs
+++ crates/peer-metrics/src/lib.rs
@@ -11,3 +11,10 @@ pub use connectivity::Resolution;
 pub use dispatcher::DispatcherMetrics;
 pub use particle_executor::{ParticleExecutorMetrics, ServiceCall};
 pub use vm_pool::VmPoolMetrics;
+
+// TODO:
+// - service creation time
+// - number of services
+// - heap statistics
+// - individual actor mailbox size: max and histogram
+// - count 'Error processing inbound ProtocolMessage: unexpected end of file'
