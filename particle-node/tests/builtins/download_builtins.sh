908ac88fd72e2ffe652fce8e852ae29ac9322f00
folex <0xdxdy@gmail.com>
2022-03-01 13:45:45 +0300

    Revert "aqua-dht => registry"
    This reverts commit 770a1da6292524f0247f3203be2f231aa85d90de.

--- particle-node/tests/builtins/download_builtins.sh
+++ particle-node/tests/builtins/download_builtins.sh
@@ -4,10 +4,10 @@ set -o pipefail -o nounset -o errexit
 # get script directory
 BUILTINS_DIR="$(dirname "$0")"
 SERVICES_DIR="${BUILTINS_DIR}/services"
-TAR="registry.tar.gz"
+TAR="aqua-dht.tar.gz"
 
 echo "*** downloading $TAR ***"
-URL="https://github.com/fluencelabs/registry/releases/latest/download/registry.tar.gz"
+URL="https://github.com/fluencelabs/aqua-dht/releases/latest/download/aqua-dht.tar.gz"
 curl --fail -L "$URL" -o "$TAR"
 
 echo "*** extracting $TAR ***"
