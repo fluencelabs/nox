b56e438bcc81e6e53b06bcfe5dbfbd4eda91fdf3
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2022-04-06 18:06:34 +0400

    yamux default

--- crates/libp2p/src/transport.rs
+++ crates/libp2p/src/transport.rs
@@ -73,7 +73,7 @@ where
     let multiplex = {
         let mut mplex = libp2p::mplex::MplexConfig::default();
         mplex.set_max_num_streams(1024 * 1024);
-        let mut yamux = libp2p::yamux::YamuxConfig::server();
+        let mut yamux = libp2p::yamux::YamuxConfig::default();
         yamux.set_max_num_streams(1024 * 1024);
 
         // // 1MB
