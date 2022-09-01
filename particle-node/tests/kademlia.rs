ac76c923cdbda909e449d3b65a4de026e726f3fc
folex <0xdxdy@gmail.com>
2022-09-01 15:31:49 +0400

    add test neighborhood_with_addresses

--- particle-node/tests/kademlia.rs
+++ particle-node/tests/kademlia.rs
@@ -14,20 +14,19 @@
  * limitations under the License.
  */
 
-use connected_client::ConnectedClient;
-use created_swarm::make_swarms;
-use test_constants::KAD_TIMEOUT;
-
 use eyre::WrapErr;
+use itertools::Itertools;
 use libp2p::PeerId;
 use maplit::hashmap;
 use serde_json::{json, Value as JValue};
-use std::thread::sleep;
+
+use connected_client::ConnectedClient;
+use created_swarm::make_swarms;
+use particle_protocol::Contact;
 
 #[test]
 fn neighborhood() {
     let swarms = make_swarms(3);
-    sleep(KAD_TIMEOUT);
     let mut client = ConnectedClient::connect_to(swarms[0].multiaddr.clone())
         .wrap_err("connect client")
         .unwrap();
@@ -60,4 +59,48 @@ fn neighborhood() {
     } else {
         panic!("response[0] must be an array, response was {:#?}", response);
     }
+}
+
+#[test]
+fn neighborhood_with_addresses() {
+    let swarms = make_swarms(3);
+    let mut client = ConnectedClient::connect_to(swarms[0].multiaddr.clone())
+        .wrap_err("connect client")
+        .unwrap();
+
+    client.send_particle(
+        r#"
+            (seq
+                (call node ("kad" "neigh_with_addrs") [node] peers)
+                (call client ("return" "") [peers] void)
+            )
+        "#,
+        hashmap! {
+            "node" => json!(client.node.to_string()),
+            "client" => json!(client.peer_id.to_string())
+        },
+    );
+    let response = client.receive_args().wrap_err("receive").unwrap();
+    let neighborhood = response.into_iter().next().expect("empty response");
+    let neighborhood: Vec<Contact> =
+        serde_json::from_value(neighborhood).expect("deserialize neighborhood");
+    assert_eq!(neighborhood.len(), 2);
+
+    let first = neighborhood
+        .iter()
+        .find(|c| c.peer_id == swarms[1].peer_id)
+        .expect("1st node wasn't found in neighborhood");
+    assert!(
+        first.addresses.iter().contains(&swarms[1].multiaddr),
+        "1st node's multiaddr not found in contact"
+    );
+
+    let second = neighborhood
+        .iter()
+        .find(|c| c.peer_id == swarms[2].peer_id)
+        .expect("2nd node wasn't found in neighborhood");
+    assert!(
+        second.addresses.iter().contains(&swarms[2].multiaddr),
+        "2nd node's multiaddr not found in contact"
+    );
 }
