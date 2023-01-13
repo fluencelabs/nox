bacfe586cd242cebcd4eeb049c427805101e899d
Anatoly Laskaris <github_me@nahsi.dev>
2023-01-13 14:24:06 +0200

    Run cargo fmt

--- crates/builtins-tests/tests/src/builtins_deployer.rs
+++ crates/builtins-tests/tests/src/builtins_deployer.rs
@@ -94,7 +94,11 @@ fn builtins_replace_old() {
     let tetraplets_service = create_service(
         &mut client,
         "tetraplets",
-        load_module("../../../particle-node-tests/tests/tetraplets/artifacts", "tetraplets").expect("load module"),
+        load_module(
+            "../../../particle-node-tests/tests/tetraplets/artifacts",
+            "tetraplets",
+        )
+        .expect("load module"),
     );
 
     client.send_particle(
