4c0a80d6086eeec5ade1b19e10297edb2b7719da
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2023-05-08 18:23:59 +0400

    pr fixes

--- crates/builtins-tests/download_builtins.sh
+++ crates/builtins-tests/download_builtins.sh
@@ -2,18 +2,18 @@
 set -o pipefail -o nounset -o errexit
 
 # get script directory
-#BUILTINS_DIR="$(dirname "$0")"
-#SERVICES_DIR="${BUILTINS_DIR}/../particle-node-tests/tests/builtins/services"
-#TAR="registry.tar.gz"
-#
-#echo "*** downloading $TAR ***"
-#URL="https://github.com/fluencelabs/registry/releases/download/v0.5.5/registry.tar.gz"
-#curl --fail -L "$URL" -o "$TAR"
-#
-#echo "*** extracting $TAR ***"
-#mkdir -p "$SERVICES_DIR"
-#tar -C "$SERVICES_DIR" -xf "$TAR"
-#
-#rm "$TAR"
+BUILTINS_DIR="$(dirname "$0")"
+SERVICES_DIR="${BUILTINS_DIR}/../particle-node-tests/tests/builtins/services"
+TAR="registry.tar.gz"
+
+echo "*** downloading $TAR ***"
+URL="https://github.com/fluencelabs/registry/releases/download/v0.8.5/registry.tar.gz"
+curl --fail -L "$URL" -o "$TAR"
+
+echo "*** extracting $TAR ***"
+mkdir -p "$SERVICES_DIR"
+tar -C "$SERVICES_DIR" -xf "$TAR"
+
+rm "$TAR"
 
 echo "*** done ***"
