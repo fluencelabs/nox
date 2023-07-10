82424387537d44f5c3aebc6d060766d83739df34
folex <0xdxdy@gmail.com>
2023-07-10 15:04:00 -0600

    feat: introduce enable_logs_for

--- crates/log-utils/src/lib.rs
+++ crates/log-utils/src/lib.rs
@@ -14,69 +14,128 @@
  * limitations under the License.
  */
 
-use tracing_subscriber::filter::LevelFilter;
+use log::Level;
+use tracing_subscriber::filter::{Directive, LevelFilter};
+
+fn default_directives() -> Vec<Directive> {
+    let namespaces = vec![
+        "script_storage=trace",
+        "run-console=trace",
+        "sorcerer=trace",
+        "key_manager=trace",
+        "spell_event_bus=trace",
+        "aquamarine=trace",
+        "network=trace",
+        "network_api=trace",
+        "aquamarine::actor=debug",
+        "nox::bootstrapper=info",
+        "yamux::connection::stream=info",
+        "tokio_threadpool=info",
+        "tokio_reactor=info",
+        "mio=info",
+        "tokio_io=info",
+        "soketto=info",
+        "yamix=info",
+        "multistream_select=info",
+        "libp2p_swarm=info",
+        "libp2p_secio=info",
+        "libp2p_websocket::framed=info",
+        "libp2p_ping=info",
+        "libp2p_core::upgrade::apply=info",
+        "libp2p_kad::kbucket=info",
+        "libp2p_kad=info",
+        "libp2p_kad::query=info",
+        "libp2p_kad::iterlog=info",
+        "libp2p_plaintext=info",
+        "libp2p_identify::protocol=info",
+        "cranelift_codegen=off",
+        "cranelift_codegen::context=off",
+        "wasmer_wasi=info",
+        "wasmer_interface_types_fl=info",
+        "polling=info",
+        "walrus=info",
+        "regalloc2=info",
+        "cranelift_wasm=info",
+        "wasmtime_cranelift=info",
+        "tokio=info",
+        "libp2p_noise=info",
+        "yamux=info",
+        "wasmtime_jit=info",
+        "wasi_common=info",
+        "particle_reap=info",
+        "marine_core::module::marine_module=info",
+        "runtime::resource=info",
+    ];
+
+    namespaces
+        .into_iter()
+        .map(|ns| {
+            ns.parse()
+                .expect(format!("cannot parse {ns} to Directive").as_str())
+        })
+        .collect()
+}
 
 #[allow(dead_code)]
 // Enables logging, filtering out unnecessary details
 pub fn enable_logs() {
-    std::env::set_var("WASM_LOG", "info");
-
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            tracing_subscriber::EnvFilter::builder()
-                .with_default_directive(LevelFilter::TRACE.into())
-                .from_env_lossy()
-                .add_directive("script_storage=trace".parse().unwrap())
-                .add_directive("run-console=trace".parse().unwrap())
-                .add_directive("sorcerer=trace".parse().unwrap())
-                .add_directive("key_manager=trace".parse().unwrap())
-                .add_directive("spell_event_bus=trace".parse().unwrap())
-                .add_directive("aquamarine=trace".parse().unwrap())
-                .add_directive("network=trace".parse().unwrap())
-                .add_directive("network_api=trace".parse().unwrap())
-                .add_directive("aquamarine::actor=debug".parse().unwrap())
-                .add_directive("nox::bootstrapper=info".parse().unwrap())
-                .add_directive("yamux::connection::stream=info".parse().unwrap())
-                .add_directive("tokio_threadpool=info".parse().unwrap())
-                .add_directive("tokio_reactor=info".parse().unwrap())
-                .add_directive("mio=info".parse().unwrap())
-                .add_directive("tokio_io=info".parse().unwrap())
-                .add_directive("soketto=info".parse().unwrap())
-                .add_directive("yamix=info".parse().unwrap())
-                .add_directive("multistream_select=info".parse().unwrap())
-                .add_directive("libp2p_swarm=info".parse().unwrap())
-                .add_directive("libp2p_secio=info".parse().unwrap())
-                .add_directive("libp2p_websocket::framed=info".parse().unwrap())
-                .add_directive("libp2p_ping=info".parse().unwrap())
-                .add_directive("libp2p_core::upgrade::apply=info".parse().unwrap())
-                .add_directive("libp2p_kad::kbucket=info".parse().unwrap())
-                .add_directive("libp2p_kad=info".parse().unwrap())
-                .add_directive("libp2p_kad::query=info".parse().unwrap())
-                .add_directive("libp2p_kad::iterlog=info".parse().unwrap())
-                .add_directive("libp2p_plaintext=info".parse().unwrap())
-                .add_directive("libp2p_identify::protocol=info".parse().unwrap())
-                .add_directive("cranelift_codegen=off".parse().unwrap())
-                .add_directive("cranelift_codegen::context=off".parse().unwrap())
-                .add_directive("wasmer_wasi=info".parse().unwrap())
-                .add_directive("wasmer_interface_types_fl=info".parse().unwrap())
-                .add_directive("polling=info".parse().unwrap())
-                .add_directive("walrus=info".parse().unwrap())
-                .add_directive("regalloc2=info".parse().unwrap())
-                .add_directive("cranelift_wasm=info".parse().unwrap())
-                .add_directive("wasmtime_cranelift=info".parse().unwrap())
-                .add_directive("tokio=info".parse().unwrap())
-                .add_directive("libp2p_noise=info".parse().unwrap())
-                .add_directive("yamux=info".parse().unwrap())
-                .add_directive("wasmtime_jit=info".parse().unwrap())
-                .add_directive("wasi_common=info".parse().unwrap())
-                .add_directive("particle_reap=info".parse().unwrap())
-                .add_directive("marine_core::module::marine_module=info".parse().unwrap())
-                .add_directive("runtime::resource=info".parse().unwrap()),
-        )
-        .try_init()
-        .ok();
+    enable_logs_for(LogSpec::default())
 }
 
 pub fn enable_console() {
     console_subscriber::init();
+}
+
+pub struct LogSpec {
+    directives: Vec<Directive>,
+    wasm_log: Level,
+}
+
+impl Default for LogSpec {
+    fn default() -> Self {
+        Self::new(vec![])
+            .with_defaults()
+            .with_wasm_level(Level::Info)
+    }
+}
+
+impl LogSpec {
+    pub fn new(directives: Vec<Directive>) -> Self {
+        Self {
+            directives,
+            wasm_log: Level::Info,
+        }
+    }
+
+    pub fn with_defaults(mut self) -> Self {
+        self.directives = default_directives()
+            .into_iter()
+            .chain(self.directives)
+            .collect();
+
+        self
+    }
+
+    pub fn with_wasm_level(mut self, level: Level) -> Self {
+        self.wasm_log = level;
+
+        self
+    }
+}
+
+pub fn enable_logs_for(spec: LogSpec) {
+    std::env::set_var("WASM_LOG", spec.wasm_log.to_string().to_lowercase());
+
+    let mut filter = tracing_subscriber::EnvFilter::builder()
+        .with_default_directive(LevelFilter::TRACE.into())
+        .from_env_lossy();
+
+    for d in spec.directives {
+        filter = filter.add_directive(d);
+    }
+
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .try_init()
+        .ok();
 }
