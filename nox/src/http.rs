4c495170f180d28306cdc263a447a61cb0ea2849
Nick Pavlov <gurinderu@gmail.com>
2023-07-14 14:58:15 +0300

    Add tests

--- nox/src/http.rs
+++ nox/src/http.rs
@@ -93,3 +93,97 @@ pub async fn start_http_endpoint(
         .await
         .expect("Could not make http endpoint")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::http::Request;
+    use std::net::{SocketAddr, TcpListener};
+    use tokio::time::sleep;
+
+    fn get_free_tcp_port() -> u16 {
+        let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to port 0");
+        let socket_addr = listener
+            .local_addr()
+            .expect("Failed to retrieve local address");
+        let port = socket_addr.port();
+        drop(listener);
+        port
+    }
+
+    fn test_versions() -> Versions {
+        Versions {
+            node_version: "node_test_version".to_string(),
+            avm_version: "avm_test_version".to_string(),
+            spell_version: "spell_test_version".to_string(),
+            system_service: system_services::Versions {
+                aqua_ipfs_version: "aqua_ipfs_test_version",
+                trust_graph_version: "trust_graph_test_version",
+                registry_version: "registry_test_version",
+                decider_version: "decider_test_version",
+            },
+        }
+    }
+
+    #[tokio::test]
+    async fn test_version_route() {
+        // Create a test server
+        let port = get_free_tcp_port();
+        let addr = format!("127.0.0.1:{port}").parse::<SocketAddr>().unwrap();
+
+        tokio::spawn(async move {
+            start_http_endpoint(addr, None, PeerId::random(), test_versions()).await;
+        });
+
+        sleep(tokio::time::Duration::from_secs(1)).await;
+
+        let client = hyper::Client::new();
+
+        let response = client
+            .request(
+                Request::builder()
+                    .uri(format!("http://{}/versions", addr))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let status = response.status();
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(status, StatusCode::OK);
+        assert_eq!(&body[..], b"{\"node\":\"node_test_version\",\"avm\":\"avm_test_version\",\"spell\":\"spell_test_version\",\"aqua_ipfs\":\"aqua_ipfs_test_version\",\"trust_graph\":\"trust_graph_test_version\",\"registry\":\"registry_test_version\",\"decider\":\"decider_test_version\"}");
+    }
+
+    #[tokio::test]
+    async fn test_peer_id_route() {
+        // Create a test server
+        let port = get_free_tcp_port();
+        let addr = format!("127.0.0.1:{port}").parse::<SocketAddr>().unwrap();
+        let peer_id = PeerId::random();
+
+        tokio::spawn(async move {
+            start_http_endpoint(addr, None, peer_id, test_versions()).await;
+        });
+
+        sleep(tokio::time::Duration::from_secs(1)).await;
+
+        let client = hyper::Client::new();
+
+        let response = client
+            .request(
+                Request::builder()
+                    .uri(format!("http://{}/peer_id", addr))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let status = response.status();
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(status, StatusCode::OK);
+        assert_eq!(
+            &body[..],
+            format!("{{\"peer_id\":\"{}\"}}", peer_id).as_bytes()
+        );
+    }
+}
