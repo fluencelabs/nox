ba52a398083f92f839132cdde933eac59be5adb4
Nick Pavlov <gurinderu@gmail.com>
2023-07-26 20:04:23 +0300

    Review fixes

--- aquamarine/src/health.rs
+++ aquamarine/src/health.rs
@@ -1,30 +1,30 @@
 use health::HealthCheck;
-use parking_lot::Mutex;
+use parking_lot::RwLock;
 use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct VMPoolHealth {
     expected_count: usize,
-    current_count: Arc<Mutex<usize>>,
+    current_count: Arc<RwLock<usize>>,
 }
 
 impl VMPoolHealth {
     pub fn new(expected_count: usize) -> Self {
         Self {
             expected_count,
-            current_count: Arc::new(Mutex::new(0)),
+            current_count: Arc::new(RwLock::new(0)),
         }
     }
 
     pub fn increment_count(&self) {
-        let mut guard = self.current_count.lock();
+        let mut guard = self.current_count.write();
         *guard += 1;
     }
 }
 
 impl HealthCheck for VMPoolHealth {
     fn status(&self) -> eyre::Result<()> {
-        let guard = self.current_count.lock();
+        let guard = self.current_count.read();
         let current = *guard;
         if self.expected_count != current {
             return Err(eyre::eyre!(
