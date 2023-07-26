b90a5d1d21ae1dcb316d6da0cc8b1c7a1d41f82c
Nick Pavlov <gurinderu@gmail.com>
2023-07-26 20:40:55 +0300

    Change counter to atomic

--- aquamarine/src/health.rs
+++ aquamarine/src/health.rs
@@ -1,31 +1,29 @@
 use health::HealthCheck;
-use parking_lot::RwLock;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Clone)]
 pub struct VMPoolHealth {
     expected_count: usize,
-    current_count: Arc<RwLock<usize>>,
+    current_count: Arc<AtomicUsize>,
 }
 
 impl VMPoolHealth {
     pub fn new(expected_count: usize) -> Self {
         Self {
             expected_count,
-            current_count: Arc::new(RwLock::new(0)),
+            current_count: Arc::new(AtomicUsize::new(0)),
         }
     }
 
     pub fn increment_count(&self) {
-        let mut guard = self.current_count.write();
-        *guard += 1;
+        self.current_count.fetch_add(1, Ordering::Release);
     }
 }
 
 impl HealthCheck for VMPoolHealth {
     fn status(&self) -> eyre::Result<()> {
-        let guard = self.current_count.read();
-        let current = *guard;
+        let current = self.current_count.load(Ordering::Acquire);
         if self.expected_count != current {
             return Err(eyre::eyre!(
                 "VM pool isn't full. Current: {}, Expected: {}",
