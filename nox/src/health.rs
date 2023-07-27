bad58a6ac8a634c60efb062ea88418588d5f70a4
Nick Pavlov <gurinderu@gmail.com>
2023-07-27 15:52:47 +0300

    Add tests for health checks

--- nox/src/health.rs
+++ nox/src/health.rs
@@ -49,3 +49,67 @@ impl HealthCheck for BootstrapNodesHealth {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+
+    #[test]
+    fn test_bootstrap_nodes_health_all_connected() {
+        let bootstrap_nodes = vec![
+            "/ip4/127.0.0.1/tcp/5000".parse().unwrap(),
+            "/ip4/127.0.0.1/tcp/5001".parse().unwrap(),
+        ];
+        let bootstrap_health = BootstrapNodesHealth::new(bootstrap_nodes.clone());
+
+        // Simulate connections to the bootstrap nodes
+        bootstrap_health.on_bootstrap_connected(bootstrap_nodes[0].clone());
+        bootstrap_health.on_bootstrap_connected(bootstrap_nodes[1].clone());
+
+        let status = bootstrap_health.status();
+        assert!(status.is_ok());
+    }
+
+    #[test]
+    fn test_bootstrap_nodes_health_partial_connected() {
+        let bootstrap_nodes = vec![
+            "/ip4/127.0.0.1/tcp/5000".parse().unwrap(),
+            "/ip4/127.0.0.1/tcp/5001".parse().unwrap(),
+        ];
+        let bootstrap_health = BootstrapNodesHealth::new(bootstrap_nodes.clone());
+
+        // Simulate connections to only one of the bootstrap nodes
+        bootstrap_health.on_bootstrap_connected(bootstrap_nodes[0].clone());
+
+        let status = bootstrap_health.status();
+        assert!(status.is_err());
+    }
+
+    #[test]
+    fn test_bootstrap_nodes_health_concurrent_access() {
+        let bootstrap_nodes = vec![
+            "/ip4/127.0.0.1/tcp/5000".parse().unwrap(),
+            "/ip4/127.0.0.1/tcp/5001".parse().unwrap(),
+        ];
+        let bootstrap_health = Arc::new(BootstrapNodesHealth::new(bootstrap_nodes.clone()));
+        let health_clone = bootstrap_health.clone();
+
+        let bootstrap_node = bootstrap_nodes[0].clone();
+        // Simulate concurrent access by spawning multiple threads.
+        let thread_handle = thread::spawn(move || {
+            let health = health_clone;
+            // Simulate connections to the bootstrap nodes in separate threads.
+            health.on_bootstrap_connected(bootstrap_node);
+        });
+
+        // Simulate connections to the bootstrap nodes in the main thread.
+        bootstrap_health.on_bootstrap_connected(bootstrap_nodes[1].clone());
+
+        // Wait for the spawned thread to finish.
+        thread_handle.join().unwrap();
+
+        let status = bootstrap_health.status();
+        assert!(status.is_ok());
+    }
+}
