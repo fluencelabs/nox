525d7cb109b677fc47eff5243038160b2955911d
Maria Kuklina <maria@fluence.one>
2024-03-11 16:11:27 +0100

    fix tests

--- crates/nox-tests/tests/workers.rs
+++ crates/nox-tests/tests/workers.rs
@@ -136,10 +136,13 @@ async fn test_resolve_subnet_on_worker() {
 
     let swarms = make_swarms(1).await;
 
-    let mut client = ConnectedClient::connect_to(swarms[0].multiaddr.clone())
-        .await
-        .wrap_err("connect client")
-        .unwrap();
+    let mut client = ConnectedClient::connect_with_keypair(
+        swarms[0].multiaddr.clone(),
+        Some(swarms[0].management_keypair.clone()),
+    )
+    .await
+    .wrap_err("connect client")
+    .unwrap();
 
     let worker_id = create_worker(&mut client, deal_id).await;
 
