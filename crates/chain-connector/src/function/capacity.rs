5ba2566c647d7899c36bcd4c7e7423caef69c463
Aleksey Proshutinskiy <justprosh@users.noreply.github.com>
2024-03-19 01:52:06 +0200

    fix(connector): update contracts (#2186)

--- crates/chain-connector/src/function/capacity.rs
+++ crates/chain-connector/src/function/capacity.rs
@@ -26,12 +26,6 @@ sol! {
 
         function getGlobalNonce() external view returns (bytes32);
 
-        /// @dev Returns the min required randomX proofs per epoch for the 1 CU.
-        /// @dev  If lower than this - CU is failed and CC slashed.
-        function minProofsPerEpoch() external view returns (uint256);
-
-        /// @dev Returns the max randomX proofs per epoch
-        function maxProofsPerEpoch() external view returns (uint256);
 
         /// @dev Returns the commitment status
         /// @param commitmentId Commitment id
