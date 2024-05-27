65566c67a27f33d0ccd405eb246b99f21b806c53
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2024-05-27 11:51:46 +0300

    chore: update copyright

--- crates/chain-listener/src/persistence.rs
+++ crates/chain-listener/src/persistence.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Fluence Labs Limited
+ * Copyright 2024 Fluence DAO
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
+
 use alloy_primitives::U256;
 use eyre::Context;
 use std::path::Path;
