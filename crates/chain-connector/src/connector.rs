3ab28969c6b60551a1e5bdd5b85cc6070828d58a
folex <0xdxdy@gmail.com>
2024-08-14 17:42:11 +0300

    fix(connector): prefix deal id with 0x in setWorker call (#2348)

--- crates/chain-connector/src/connector.rs
+++ crates/chain-connector/src/connector.rs
@@ -356,7 +356,7 @@ impl HttpChainConnector {
         }
         .abi_encode();
         tracing::debug!(target: "chain-connector", "Registering worker {worker_id} for deal {deal_id} with cu_id {cu_id}");
-        self.send_tx(data, deal_id.as_str()).await
+        self.send_tx(data, &deal_id.to_address()).await
     }
 
     fn difficulty_params(&self) -> ArrayParams {
