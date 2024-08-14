a180a5c642fc0499c7c93ee87bbe8dc3471a53b7
Maria Kuklina <101095419+kmd-fl@users.noreply.github.com>
2024-08-14 16:38:37 +0200

    feat(vm): add vm builtins for reboot, reset and status [BRND-23] (#2340)

--- crates/vm-utils/src/lib.rs
+++ crates/vm-utils/src/lib.rs
@@ -2,8 +2,12 @@ mod vm_utils;
 
 pub use nonempty::NonEmpty;
 pub use vm_utils::create_domain;
+pub use vm_utils::reboot_vm;
 pub use vm_utils::remove_domain;
+pub use vm_utils::reset_vm;
 pub use vm_utils::start_vm;
+pub use vm_utils::status_vm;
 pub use vm_utils::stop_vm;
 pub use vm_utils::CreateVMDomainParams;
-pub use vm_utils::VMUtilsError;
+pub use vm_utils::VmError;
+pub use vm_utils::VmStatus;
