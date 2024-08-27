28f075cd8af363b5c7aa4b5c0fc1779b6c4eebca
Maria Kuklina <maria@fluence.one>
2024-08-27 17:42:30 +0200

    add IOMMU groups

--- crates/gpu-utils/src/lib.rs
+++ crates/gpu-utils/src/lib.rs
@@ -1,5 +1,6 @@
 use pci_info::pci_enums::PciDeviceClass::DisplayController;
 use pci_info::{PciDeviceEnumerationError, PciInfo, PciInfoError, PciInfoPropertyError};
+use std::collections::{HashMap, HashSet};
 use thiserror::Error;
 
 pub use pci_info::PciLocation;
@@ -18,15 +19,42 @@ pub enum PciError {
 
 pub fn get_gpu_pci() -> Result<Vec<PciLocation>, PciError> {
     let info = PciInfo::enumerate_pci()?;
-    let mut result = Vec::new();
+    let mut gpu_devices = Vec::new();
     for device in info {
         let device = device?;
         let device_class = process_property_result(device.device_class())?;
         if device_class == DisplayController {
-            result.push(process_property_result(device.location())?);
+            gpu_devices.push(process_property_result(device.location())?);
         }
     }
-    Ok(result)
+
+    match get_iommu_groups() {
+        Ok(iommu_groups) => {
+            let result = iommu_groups
+                .iter()
+                .flat_map(|(_, devices)| {
+                    gpu_devices
+                        .iter()
+                        .filter_map(|gpu_device| {
+                            if devices.contains(gpu_device) {
+                                Some(devices.clone())
+                            } else {
+                                None
+                            }
+                        })
+                        .flatten()
+                        .collect::<HashSet<_>>()
+                })
+                .collect::<Vec<_>>();
+            Ok(result)
+        }
+        Err(err) => {
+            tracing::warn!(
+                "Couldn't get IOMMU groups: {err}. Ignoring groups, provide list of PCI nevertheless: {gpu_devices:?}",
+            );
+            Ok(gpu_devices)
+        }
+    }
 }
 
 fn process_property_result<T>(result: Result<T, &PciInfoPropertyError>) -> Result<T, PciError> {
@@ -35,4 +63,47 @@ fn process_property_result<T>(result: Result<T, &PciInfoPropertyError>) -> Resul
         Err(PciInfoPropertyError::Unsupported) => Err(PciError::UnsupportedProperty),
         Err(PciInfoPropertyError::Error(err)) => Err(PciError::PciInfoPropertyError(err.clone())),
     }
+}
+
+type IommuGroup = HashMap<String, HashSet<PciLocation>>;
+
+fn get_iommu_groups() -> Result<IommuGroup, std::io::Error> {
+    const IOMMU_GROUP_PATH: &str = "/sys/kernel/iommu_groups";
+    let mut devices_by_group = HashMap::new();
+
+    for iommu_group_result in std::fs::read_dir(IOMMU_GROUP_PATH)? {
+        if let Ok(iommu_group) = iommu_group_result {
+            let group_name = iommu_group.file_name().to_string_lossy().to_string();
+
+            let path = iommu_group.path().join("devices");
+
+            let group = std::fs::read_dir(path)?
+                .flatten()
+                .filter_map(|device| {
+                    parse_pci_location(device.file_name().to_string_lossy().to_string())
+                })
+                .collect::<HashSet<_>>();
+
+            devices_by_group.insert(group_name, group);
+        } else {
+            tracing::warn!("cannot get an IOMMU group: {:?}", iommu_group_result);
+        }
+    }
+
+    Ok(devices_by_group)
+}
+
+// Location format:
+// <segment>:<bus>:<device>.<function>: 0000:00:00.0
+fn parse_pci_location(location: String) -> Option<PciLocation> {
+    let parts: Vec<&str> = location.split(':').collect();
+    if parts.len() != 3 {
+        return None;
+    }
+    // the segment part is always 0
+    let bus = u8::from_str_radix(parts[1], 16).ok()?;
+    let device = u8::from_str_radix(parts[2].split('.').next()?, 16).ok()?;
+    let function = u8::from_str_radix(parts[2].split('.').last()?, 16).ok()?;
+
+    PciLocation::with_bdf(bus, device, function).ok()
 }
---
+++ crates/vm-network-utils/bin/main.rs
@@ -0,0 +1,18 @@
+use std::net::Ipv4Addr;
+use std::str::FromStr;
+use vm_network_utils::{clear_network, setup_network, NetworkSettings};
+
+// bin for local tests
+fn main() {
+    let ns = NetworkSettings {
+        public_ip: Ipv4Addr::from_str("1.1.1.1").unwrap(),
+        vm_ip: Ipv4Addr::from_str("2.2.2.2").unwrap(),
+        bridge_name: "br0".to_string(),
+        port_range: (1000, 65535),
+        host_ssh_port: 2222,
+        vm_ssh_port: 22,
+    };
+    //let result = setup_network(&ns, "test");
+    let result = clear_network(&ns, "12D3KooWAb7dquiiyrxZEchx");
+    println!("{result:?}");
+}
